b198a79be810e2dab4b759dbeb0190602dc5b830
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2019-09-17 11:29:19 +0200

    Bring back the utilities

---
+++ src/app/Utils.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Kolab;
+
+use Ramsey\Uuid\Uuid;
+
+class Utils
+{
+    /**
+        Returns an integer derived from a UUID.
+
+        @return integer
+     */
+    public static function uuidInt()
+    {
+        $hex = Uuid::uuid4();
+        $bin = pack('h*', str_replace('-', '', $hex));
+        $ids = unpack('L', $bin);
+        $id = array_shift($ids);
+
+        return $id;
+    }
+
+    /**
+        Returns a UUID in the form of a string.
+
+        @return string
+     */
+    public static function uuidStr()
+    {
+        return (string) Uuid::uuid4();
+    }
+}
