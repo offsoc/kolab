3241fa45151a93ace472bb60d9131527e8c9d2f5
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2019-09-17 11:49:09 +0200

    Euh, data please

--- src/resources/vue/components/DashboardComponent.vue
+++ src/resources/vue/components/DashboardComponent.vue
@@ -11,7 +11,7 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">Dashboard</div>
                     <div class="panel-body">
-                        <p>Data: ""</p>
+                        <p>Data: {{ data }}</p>
                     </div>
                 </div>
 
