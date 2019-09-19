7104bdfffa6ec3939e6480c2fde32296c41f29ec
Aleksander Machniak <machniak@kolabsys.com>
2019-09-19 12:26:54 +0200

    Remove example component

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -8,14 +8,6 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
 import router from '../vue/js/routes.js';
 
 import AppComponent from '../vue/components/AppComponent'
