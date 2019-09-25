e9ba2db532ef781e5579130d099fa650dfa9622b
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2019-09-25 07:39:37 +0200

    Fix setting uid=kolab-service password

--- docker/kolab/utils/01-reset-kolab-service-password.sh
+++ docker/kolab/utils/01-reset-kolab-service-password.sh
@@ -8,4 +8,6 @@
     echo "replace: userpassword"
     echo "userpassword: ${ldap_bindpw}"
     echo ""
-) | ldapmodify -x -h ${ldap_host} -D "${ldap_binddn}" -w "${ldap_bindpw}" -f -
+) | ldapmodify -x -h ${ldap_host} -D "${ldap_binddn}" -w "${ldap_bindpw}"
+
+sed -i -r -e "s/^service_bind_pw = .*$/service_bind_pw = ${ldap_bindpw}/g" /etc/kolab/kolab.conf
