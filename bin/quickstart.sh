803f8165292b2ea9735d6b365847f38073b69fcc
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2019-09-25 15:02:17 +0200

    Ensure everything's up and running before we seed the database

--- bin/quickstart.sh
+++ bin/quickstart.sh
@@ -10,18 +10,26 @@ docker pull kolab/centos7:latest
 
 docker-compose down
 docker-compose build
-docker-compose up -d
+
+docker-compose up -d kolab mariadb redis
 
 pushd ${base_dir}/src/
 composer install
 npm install
+rm -rf bootstrap/cache/
+mkdir bootstrap/cache/
 cp .env.example .env
 ./artisan key:generate
 ./artisan jwt:secret -f
 ./artisan clear-compiled
 npm run dev
+popd
+
+docker-compose up -d worker
+
+pushd ${base_dir}/src/
 rm -rf database/database.sqlite
-touch database/database.sqlite
 ./artisan migrate:refresh --seed
 ./artisan serve
 popd
+
