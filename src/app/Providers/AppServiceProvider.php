c73767dec4419fa11f8f2083a1eeb0a690cf625c
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2019-10-02 10:04:06 +0200

    Also update AppServiceProvider for new Observers

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -30,13 +30,27 @@ class AppServiceProvider extends ServiceProvider
         \App\SignupCode::observe(\App\Observers\SignupCodeObserver::class);
         \App\Sku::observe(\App\Observers\SkuObserver::class);
         \App\User::observe(\App\Observers\UserObserver::class);
+        \App\UserSetting::observe(\App\Observers\UserSettingObserver::class);
         \App\Wallet::observe(\App\Observers\WalletObserver::class);
 
         // Log SQL queries in debug mode
         if (env('APP_DEBUG')) {
-            DB::listen(function($query) {
-                \Log::debug(sprintf('[SQL] %s [%s]', $query->sql, implode(', ', $query->bindings)));
-            });
+            DB::listen(
+                function ($query) {
+                    \Log::debug(
+                        sprintf('[SQL] %s [%s]', $query->sql, implode(', ', $query->bindings))
+                    );
+                }
+            );
+
+            DB::listen(
+                function ($query) {
+                    File::append(
+                        storage_path('/logs/sql.log'),
+                        $query->sql . ' [' . implode(', ', $query->bindings) . ']' . PHP_EOL
+                    );
+                }
+            );
         }
     }
 }
