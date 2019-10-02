2d785b106f72edc2e794aae74b2fbbecfb871b77
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2019-10-02 16:12:44 +0200

    Delete the existing mappings

---
+++ bin/reinit-logstash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for index in $(curl localhost:9200/_cat/indices 2>/dev/null | awk '{print $3}' | grep ^logstash)
+do
+    curl -X DELETE localhost:9200/${index} >/dev/null 2>&1
+done
+
+find docker/logstash/_grokparsefailures/ -type f ! -name ".gitignore" -delete
+
+curl -X DELETE localhost:9200/_template/logstash-1.0.0 >/dev/null 2>&1
+
+curl -X PUT -H 'Content-Type: application/json' \
+    -d "$(cat docker/logstash/templates/logstash_template.json)" \
+    localhost:9200/_template/logstash-1.0.0 >/dev/null 2>&1
+
+docker-compose up -d --force-recreate logstash
+docker-compose logs -f logstash
