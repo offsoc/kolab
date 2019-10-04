48a5c682d17d651fa1ee9e24ffff7ce9a0410e35
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2019-10-04 12:39:31 +0200

    Fix reset cyrus-admin password

--- docker/kolab/utils/11-reset-cyrus-admin-password.sh
+++ docker/kolab/utils/11-reset-cyrus-admin-password.sh
@@ -8,4 +8,13 @@
     echo "replace: userpassword"
     echo "userpassword: ${ldap_bindpw}"
     echo ""
-) | ldapmodify -x -h ${ldap_host} -D "${ldap_binddn}" -w "${ldap_bindpw}" -f -
+) | ldapmodify -x -h ${ldap_host} -D "${ldap_binddn}" -w "${ldap_bindpw}"
+
+oldpw=$(grep ^admin_password /etc/kolab/kolab.conf | awk '{print $3}')
+
+sed -i -r \
+    -e "s/${oldpw}/${ldap_bindpw}/g" \
+    /etc/kolab/kolab.conf
+
+systemctl restart kolabd wallaced
+
