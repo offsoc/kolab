921e6e6ba09f4b511732bb520c873b8f1406263b
Aleksander Machniak <machniak@kolabsys.com>
2019-10-08 15:44:05 +0200

    Add browser tests

---
+++ src/tests/Browser/LogonTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+
+use Tests\Browser\Pages\Dashboard;
+use Tests\Browser\Pages\Home;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class LogonTest extends DuskTestCase
+{
+
+    /**
+     * Test redirect to /login if user is unauthenticated
+     *
+     * @return void
+     */
+    public function testLogonRedirect()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/dashboard');
+
+            // Checks if we're really on the login page
+            $browser->waitForLocation('/login')
+                ->on(new Home);
+        });
+    }
+
+    /**
+     * Logon with wrong password/user test
+     *
+     * @return void
+     */
+    public function testLogonWrongCredentials()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new Home)
+                ->submitLogon('jdoe@example.org', 'wrong');
+
+            // Checks if we're still on the logon page
+            // FIXME: This assertion might be prone to timing issues
+            // I guess we should wait until some error message appears
+            $browser->on(new Home);
+        });
+    }
+
+    /**
+     * Successful logon test
+     *
+     * @return void
+     */
+    public function testLogonSuccessful()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new Home)
+                ->submitLogon('jdoe@example.org', 'simple123', true);
+
+            // Checks if we're really on Dashboard page
+            $browser->on(new Dashboard);
+        });
+    }
+}
---
+++ src/tests/Browser/Pages/Home.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Browser\Pages;
+
+use Laravel\Dusk\Browser;
+use Laravel\Dusk\Page;
+
+class Home extends Page
+{
+    /**
+     * Get the URL for the page.
+     *
+     * @return string
+     */
+    public function url()
+    {
+        return '/';
+    }
+
+    /**
+     * Assert that the browser is on the page.
+     *
+     * @param  \Laravel\Dusk\Browser  $browser
+     * @return void
+     */
+    public function assert(Browser $browser)
+    {
+        $browser->assertVisible('form.form-signin');
+    }
+
+    /**
+     * Get the element shortcuts for the page.
+     *
+     * @return array
+     */
+    public function elements()
+    {
+        return [
+            '@app' => '#app',
+        ];
+    }
+
+    /**
+     * Submit logon form.
+     *
+     * @param Browser $browser
+     * @param string  $username
+     * @param string  $password
+     * @param bool    $wait_for_dashboard
+     *
+     * @return void
+     */
+    public function submitLogon(Browser $browser, $username, $password, $wait_for_dashboard = false)
+    {
+        $browser
+            ->type('#inputEmail', $username)
+            ->type('#inputPassword', $password)
+            ->press('form button');
+
+        if ($wait_for_dashboard) {
+            $browser->waitForLocation('/dashboard');
+        }
+    }
+}
