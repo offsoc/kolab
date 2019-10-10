d3b1150d3b2bef9196999e0c80e5fb772949b5e5
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2019-10-10 13:07:12 +0200

    Add a function to create a power set of elements.

--- src/app/Utils.php
+++ src/app/Utils.php
@@ -10,6 +10,24 @@ use Ramsey\Uuid\Uuid;
 class Utils
 {
     /**
+     * Provide all unique combinations of elements in $input, with order and duplicates irrelevant.
+     *
+     * @param array $input The input array of elements.
+     *
+     * @return array[]
+     */
+    public static function powerSet($input)
+    {
+        $output = [];
+
+        for ($x = 0; $x < count($input); $x++) {
+            self::combine($input, $x + 1, 0, array(), 0, $output);
+        }
+
+        return $output;
+    }
+
+    /**
      * Returns a UUID in the form of an integer.
      *
      * @return integer
@@ -32,5 +50,29 @@ class Utils
     public static function uuidStr()
     {
         return (string) Uuid::uuid4();
+    }
+
+    private static function combine($input, $r, $index, $data, $i, &$output)
+    {
+        $n = count($input);
+
+        // Current cobination is ready
+        if ($index == $r) {
+            $output[] = array_slice($data, 0, $r);
+            return;
+        }
+
+        // When no more elements are there to put in data[]
+        if ($i >= $n) {
+            return;
+        }
+
+        // current is included, put next at next location
+        $data[$index] = $input[$i];
+        self::combine($input, $r, $index + 1, $data, $i + 1, $output);
+
+        // current is excluded, replace it with next (Note that i+1
+        // is passed, but index is not changed)
+        self::combine($input, $r, $index, $data, $i + 1, $output);
     }
 }
