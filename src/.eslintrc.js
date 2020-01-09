3aee46b3fa8eb411b83b1c9639062980bcc9bb3e
Aleksander Machniak <machniak@kolabsys.com>
2020-01-09 13:01:24 +0100

    Add missing stylelint and eslint config

---
+++ src/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
+  rules: {
+    "vue/attributes-order": "off",
+    "vue/html-indent": ["error", 4],
+    "vue/html-self-closing": "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/singleline-html-element-content-newline": "off"
+  }
+}
