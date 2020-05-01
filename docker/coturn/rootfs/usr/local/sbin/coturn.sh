f27396cbf9e276081bb9d409385d559436ac52b2
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2020-05-01 12:02:37 +0200

    Docker environment enhancements

---
+++ docker/coturn/rootfs/usr/local/sbin/coturn.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat > /etc/coturn/turnserver.conf << EOF
+external-ip=${TURN_PUBLIC_IP:-127.0.0.1}
+listening-port=${TURN_LISTEN_PORT:-3478}
+fingerprint
+lt-cred-mech
+max-port=${MAX_PORT:-65535}
+min-port=${MIN_PORT:-40000}
+pidfile="/run/coturn/turnserver.pid"
+realm=openvidu
+simple-log
+redis-userdb="ip=${REDIS_IP:-127.0.0.1} dbname=${DB_NAME:-1} connect_timeout=30"
+verbose
+EOF
