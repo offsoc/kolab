6170ff2e365415091ee17bafdc3eb4bed614e5ef
Jeroen van Meeuwen <vanmeeuwen@kolabsys.com>
2020-05-11 13:55:28 +0200

    Strip superfluous snippets

---
+++ src/app/Console/Commands/OpenViduRooms.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class OpenViduRooms extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'openvidu:rooms';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'List OpenVidu rooms';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $rooms = \App\OpenVidu\Room::all();
+
+        foreach ($rooms as $room) {
+            $this->info("{$room->name}");
+        }
+    }
+}
