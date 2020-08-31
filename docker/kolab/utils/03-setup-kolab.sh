8b0ea5a0628b9554319485e249bb4c6f88001274
Jeroen van Meeuwen <vanmeeuwen@kolabsys.com>
2020-08-31 17:12:32 +0200

    Two minor changes to the openshift setup

--- docker/kolab/utils/03-setup-kolab.sh
+++ docker/kolab/utils/03-setup-kolab.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
+. ./settings.sh
+
 if [ -f /root/kolab.conf.template ]; then
     eval "echo \"$(cat /root/kolab.conf.template)\"" > /root/kolab.conf.ref
     KOLAB_CONFIG_REF="--config=/root/kolab.conf.ref"
     cp -f ${KOLAB_CONFIG_REF#--config=} /etc/kolab/kolab.conf
 fi
 
-setup-kolab \
+CMD="$(which setup-kolab) \
     --default ${LDAP_HOST+--without-ldap} ${KOLAB_CONFIG_REF} \
-    --fqdn=kolab.mgmt.com  \
+    --fqdn=kolab.${domain}  \
     --timezone=Europe/Zurich \
     --mysqlhost=${DB_HOST:-127.0.0.1} \
     --mysqlserver=existing \
     --mysqlrootpw=${DB_ROOT_PASSWORD:-Welcome2KolabSystems} \
-    --directory-manager-pwd=${LDAP_ADMIN_BIND_PW:-Welcome2KolabSystems} 2>&1 | tee /root/setup-kolab.log
+    --directory-manager-pwd=${LDAP_ADMIN_BIND_PW:-Welcome2KolabSystems}"
+
+echo ${CMD} | tee -a /root/setup-kolab.log
+echo -n "Wait for MariaDB container: " | tee -a /root/setup-kolab.log
+while ! mysqladmin -u root ping > /dev/null 2>&1 ; do
+        echo -n '.'
+        sleep 3
+done | tee -a /root/setup-kolab.log
+echo "OK!" | tee -a /root/setup-kolab.log
+echo -n "Wait for DS389 container: " | tee -a /root/setup-kolab.log
+while ! ldapsearch -h ${LDAP_HOST} -D "${LDAP_ADMIN_BIND_DN}" -w "${LDAP_ADMIN_BIND_PW}" -b "" -s base > /dev/null 2>&1 ; do
+        echo -n '.'
+        sleep 3
+done | tee -a /root/setup-kolab.log
+echo "OK!" | tee -a /root/setup-kolab.log
+${CMD} 2>&1 | tee -a /root/setup-kolab.log
 
