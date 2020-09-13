5b7c755274ca6b541d33a437f4b7ead74124d307
Jeroen van Meeuwen <vanmeeuwen@kolabsys.com>
2020-09-13 11:31:49 +0200

    Add command hkccp:importldap for the synncronization to LDAP

---
+++ src/app/Console/Commands/HKCCPImportLdap.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Backends\LDAP;
+use Illuminate\Console\Command;
+use Illuminate\Database\Schema\Blueprint;
+
+class HKCCPImportLdap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'hkccp:importldap';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sync LDAP after the migration.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        LDAP::connect();
+
+        $this->syncDomains();
+        $this->syncUsers();
+
+        LDAP::disconnect();
+    }
+
+    private function syncDomains()
+    {
+        $domains = \App\Domain::withTrashed()->get();
+
+        $bar = $this->createProgressBar(count($domains), "Syncing Domains");
+
+        foreach ($domains as $domain) {
+            $bar->advance();
+
+            $result = null;
+
+            try {
+                $result = LDAP::updateDomain($domain);
+            } catch (\Exception $e) {
+                \Log::error("Domain {$domain->namespace} failed (exception)");
+                continue;
+            }
+
+            if ($result) {
+                \Log::info("Domain {$domain->namespace} updated");
+            } else {
+                \Log::error("Domain {$domain->namespace} failed");
+            }
+        }
+
+        $bar->finish();
+
+        $this->info("DONE");
+    }
+
+    private function syncUsers()
+    {
+        $users = \App\User::withTrashed()->get();
+
+        $bar = $this->createProgressBar(count($users), "Syncing Users");
+
+        foreach ($users as $user) {
+            $bar->advance();
+
+            $result = null;
+
+            try {
+                $result = LDAP::updateUser($user);
+            } catch (\Exception $e) {
+                \Log::error("User {$user->email} failed (exception)");
+                continue;
+            }
+
+            if ($result) {
+                \Log::info("User {$user->email} updated");
+            } else {
+                \Log::error("User {$user->email} failed");
+            }
+        }
+
+        $bar->finish();
+
+        $this->info("DONE");
+    }
+
+    private function createProgressBar($count, $message = null)
+    {
+        $bar = $this->output->createProgressBar($count);
+        $bar->setFormat(
+            '%current:7s%/%max:7s% [%bar%] %percent:3s%% %elapsed:7s%/%estimated:-7s% %message% '
+        );
+
+        if ($message) {
+            $bar->setMessage($message . " ...");
+        }
+
+        $bar->start();
+
+        return $bar;
+    }
+}
