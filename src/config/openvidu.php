86092445decc9cf948044152e29ce63f1a2e8d8f
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2020-10-01 15:31:11 +0200

    Revert "Add OPENVIDU_REWRITE_HOST feature"
    This reverts commit e558488518d585588a4444af50853cf06f25f3af.

--- src/config/openvidu.php
+++ src/config/openvidu.php
@@ -3,6 +3,5 @@
         'api_password' => env('OPENVIDU_API_PASSWORD', 'MY_SECRET'),
         'api_url' => env('OPENVIDU_API_URL', 'https://localhost:8443/api/'),
         'api_username' => env('OPENVIDU_API_USERNAME', 'OPENVIDUAPP'),
-        'api_verify_tls' => (bool) env('OPENVIDU_API_VERIFY_TLS', true),
-        'rewrite_host' => env('OPENVIDU_REWRITE_HOST', null),
+        'api_verify_tls' => (bool) env('OPENVIDU_API_VERIFY_TLS', true)
     ];
