5a902f0204616bb17fb892681ad894d7e995f26e
Jeroen van Meeuwen <vanmeeuwen@kolabsys.com>
2020-11-13 15:13:20 +0100

    Add 2fa connection from MFA_DSN

--- src/config/database.php
+++ src/config/database.php
@@ -43,6 +43,11 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        '2fa' => [
+            'driver' => 'mysql',
+            'url' => env('MFA_DSN')
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
