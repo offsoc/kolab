30e21dcb9167ebcae3222fec02c34f2378b14a90
Jeroen van Meeuwen <vanmeeuwen@kolabsys.com>
2020-12-24 15:20:54 +0100

    Improve intro on voice and video conferencing
    Summary: Improve intro on voice and video conferencing
    
    Test Plan: None
    
    Reviewers: #hkccp_developers, machniak
    
    Reviewed By: #hkccp_developers, machniak
    
    Subscribers: machniak, #hkccp_developers
    
    Differential Revision: https://git.kolab.org/D1990

--- src/resources/vue/Rooms.vue
+++ src/resources/vue/Rooms.vue
@@ -2,21 +2,51 @@
     <div class="container" dusk="rooms-component">
         <div id="meet-rooms" class="card">
             <div class="card-body">
-                <div class="card-title">Video chat <small><sup class="badge badge-primary">beta</sup></small></div>
+                <div class="card-title">Voice &amp; Video Conferencing <small><sup class="badge badge-primary">beta</sup></small></div>
                 <div class="card-text">
-                    <p>We are adding a much requested feature: Video Chat.
-                        The basics are working, but it is not a polished product yet.
+                    <p>
+                        Welcome to our beta program for Voice &amp; Video Conferencing.
                     </p>
-                    <p>You have one personal video chat room that only you know the location of.
-                        It is still in beta, so you can not block people from entering once they know the location
-                        and you can not throw them out, once they are in.
-                        This functionality will come later. For now, keep that in mind when you share the location
-                        of your room.
+                    <p>
+                        You have a room of your own at the URL below. This room is only open when you yourself are in
+                        attendance. Use this URL to invite people to join you.
                     </p>
-                    <p>You can access your room and invite people by sharing this link:</p>
-                    <a v-if="href" :href="href">{{ href }}</a>
-                    <p></p>
-                    <p>Keep in mind that this is still in beta and might come with some issues.
+                    <p>
+                        <a v-if="href" :href="href">{{ href }}</a>
+                    </p>
+                    <p>
+                        This is a work in progress and more features will be added over time. Current features include:
+                    </p>
+                    <p>
+                        <dl>
+                            <dt>Screen Sharing</dt>
+                            <dd>
+                                Share your screen for presentations or show-and-tell.
+                            </dd>
+
+                            <dt>Room Security</dt>
+                            <dd>
+                                Increase the room security by setting a password that attendees will need to know
+                                before they can enter, or lock the door so attendees will have to knock, and you (the
+                                moderator) can accept or deny those requests.
+                            </dd>
+
+                            <dt>Eject Attendees</dt>
+                            <dd>
+                                Eject attendees from the session in order to force them to reconnect, or address policy
+                                violations. Click the user icon for effective dismissal.
+                            </dd>
+
+                            <dt>Silent Audience Members</dt>
+                            <dd>
+                                For a webinar-style session, have people that join choose 'None' for both the
+                                microphone and the camera so as to render them silent audience members, to allow more
+                                people in to the room.
+                            </dd>
+                        </dl>
+                    </p>
+                    <p>
+                        Keep in mind that this is still in beta and might come with some issues.
                         Should you encounter any on your way, let us know by contacting support.
                     </p>
                 </div>
