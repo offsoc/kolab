d5ff0e8918e702540a13ea3b20799ae73e289d51
Christian Mollekopf <mollekopf@kolabsys.com>
2021-07-08 12:26:18 +0200

    openvidu-dev updates
    * openvidu-2.18.0
    * Fedora 34 container
    * Build a read-only copy from the host (avoids messing with permissions
      on host)

--- docker/openvidu-dev/init.sh
+++ docker/openvidu-dev/init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cd /src/openvidu/
+set -e
+cp -R /src/openvidu /src/openvidusrc
+cd /src/openvidusrc
 mvn -DskipTests=true clean install
-/usr/bin/java -jar /src/openvidu/openvidu-server/target/openvidu-server-2.16.0.jar
+/usr/bin/java -jar /src/openvidusrc/openvidu-server/target/openvidu-server-2.18.0.jar
