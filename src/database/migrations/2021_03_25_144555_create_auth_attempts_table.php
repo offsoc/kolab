0e209586f5d2357242bdd94289d9ead7c5df7367
Jeroen van Meeuwen <vanmeeuwen@kolabsys.com>
2021-08-20 17:07:36 +0200

    Don't use the ->uuid() laravel definition, just to make sure we're perfectly in line with what we use elsewhere until we transition to relying on ->uuid()

--- src/database/migrations/2021_03_25_144555_create_auth_attempts_table.php
+++ src/database/migrations/2021_03_25_144555_create_auth_attempts_table.php
@@ -14,24 +14,28 @@ class CreateAuthAttemptsTable extends Migration
      */
     public function up()
     {
-        Schema::create('auth_attempts', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->bigInteger('user_id');
-            $table->string('ip', 36);
-            $table->string('status', 36)->default('NEW');
-            $table->string('reason', 36)->nullable();
-            $table->datetime('expires_at')->nullable();
-            $table->datetime('last_seen')->nullable();
-            $table->timestamps();
+        Schema::create(
+            'auth_attempts',
+            function (Blueprint $table) {
+                $table->string('id', 36);
+                $table->bigInteger('user_id');
+                $table->string('ip', 36);
+                $table->string('status', 36)->default('NEW');
+                $table->string('reason', 36)->nullable();
+                $table->datetime('expires_at')->nullable();
+                $table->datetime('last_seen')->nullable();
+                $table->timestamps();
 
-            $table->index('updated_at');
-            $table->unique(['user_id', 'ip']);
+                $table->primary('id');
+                $table->index('updated_at');
+                $table->unique(['user_id', 'ip']);
 
-            $table->foreign('user_id')
-                ->references('id')->on('users')
-                ->onDelete('cascade')
-                ->onUpdate('cascade');
-        });
+                $table->foreign('user_id')
+                    ->references('id')->on('users')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+            }
+        );
     }
 
     /**
