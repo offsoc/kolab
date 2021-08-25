9fbc59330cfc2d59612c4460b77515544ecdf427
Christian Mollekopf <mollekopf@kolabsys.com>
2021-08-25 10:24:33 +0200

    Run our own coturn

--- docker/coturn/rootfs/usr/local/bin/coturn.sh
+++ docker/coturn/rootfs/usr/local/bin/coturn.sh
@@ -10,8 +10,8 @@ lt-cred-mech
 max-port=${MAX_PORT:-65535}
 min-port=${MIN_PORT:-40000}
 pidfile="$(pwd)/turnserver.pid"
-realm=openvidu
-simple-log
+realm=kolabmeet
+log-file=stdout
 redis-userdb="ip=${REDIS_IP:-127.0.0.1} dbname=${DB_NAME:-2} password=${DB_PASSWORD:-turn} connect_timeout=30"
 verbose
 EOF
