4e929899ff9a7f6c8e81c700c0a5dcafb98b17bd
Christian Mollekopf <mollekopf@kolabsys.com>
2021-08-25 14:06:50 +0200

    Some better debug output from coturn

--- docker/coturn/rootfs/usr/local/bin/coturn.sh
+++ docker/coturn/rootfs/usr/local/bin/coturn.sh
@@ -7,6 +7,15 @@ external-ip=${TURN_PUBLIC_IP:-127.0.0.1}
 listening-port=${TURN_LISTEN_PORT:-3478}
 fingerprint
 lt-cred-mech
+
+# Temporary for testing
+user=username1:password1
+allow-loopback-peers
+cli-password=qwerty
+
+# Disabled by default to avoid DoS attacks. Logs all bind attempts in verbose log mode (useful for debugging)
+log-binding
+
 max-port=${MAX_PORT:-65535}
 min-port=${MIN_PORT:-40000}
 pidfile="$(pwd)/turnserver.pid"
