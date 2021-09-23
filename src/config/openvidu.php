38806e5ca179bf9d94b3f6b22d22b1028e9b97cc
Aleksander Machniak <machniak@kolabsys.com>
2021-09-23 15:37:36 +0200

    app/config/openvidu.php -> app/config/meet.php

--- src/config/openvidu.php
+++
@@ -1,7 +0,0 @@
-<?php
-    return [
-        'api_password' => env('OPENVIDU_API_PASSWORD', 'MY_SECRET'),
-        'api_url' => env('MEDIASOUP_API_URL', env('OPENVIDU_API_URL', 'https://localhost:8443/api/')),
-        'api_username' => env('OPENVIDU_API_USERNAME', 'OPENVIDUAPP'),
-        'api_verify_tls' => (bool) env('OPENVIDU_API_VERIFY_TLS', true)
-    ];
