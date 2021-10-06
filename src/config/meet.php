90e55cc8d3444ae893c290a7076810526f83f4f8
Christian Mollekopf <mollekopf@kolabsys.com>
2021-10-06 14:55:55 +0200

    The new mediaserver is running on 12443

--- src/config/meet.php
+++ src/config/meet.php
@@ -1,7 +1,7 @@
 <?php
     return [
         'api_token' => env('MEET_SERVER_TOKEN', 'MY_SECRET'),
-        'api_url' => env('MEET_SERVER_URL', 'https://localhost:8443/api/'),
+        'api_url' => env('MEET_SERVER_URL', 'https://localhost:12443/api/'),
         'api_verify_tls' => (bool) env('MEET_SERVER_VERIFY_TLS', true),
         'webhook_token' => env('MEET_WEBHOOK_TOKEN', 'MY_SECRET'),
     ];
