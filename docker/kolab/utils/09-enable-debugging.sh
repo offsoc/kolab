9325313969265f33888526b9397fe44fac2942e2
Christian Mollekopf <mollekopf@kolabsys.com>
2021-12-29 11:27:36 +0100

    Improve 09-enable-debugging
    Differential Revision: https://git.kolab.org/D3283

--- docker/kolab/utils/09-enable-debugging.sh
+++ docker/kolab/utils/09-enable-debugging.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-echo "chatty: 1" >> /etc/imapd.conf
-echo "debug: 1" >> /etc/imapd.conf
+if ! grep -q "chatty" /etc/imapd.conf; then
+    echo "chatty: 1" >> /etc/imapd.conf
+fi
+
+if ! grep -q "debug" /etc/imapd.conf; then
+    echo "debug: 1" >> /etc/imapd.conf
+fi
 
 systemctl restart cyrus-imapd
 
 sed -i -r -e "s/_debug'] = (.*);/_debug'] = true;/g" /etc/roundcubemail/config.inc.php
 
-echo "FLAGS=\"--fork -l debug -d 8\"" > /etc/sysconfig/wallace
+if ! grep -q "FLAGS=\"--fork -l debug -d 8\"" /etc/sysconfig/wallace; then
+    echo "FLAGS=\"--fork -l debug -d 8\"" > /etc/sysconfig/wallace
+fi
 systemctl restart wallace
