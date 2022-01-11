8f683014c31d7872318959a1c0918d0fae5bd586
Christian Mollekopf <mollekopf@kolabsys.com>
2022-01-11 23:28:34 +0100

    Resourcemanagement patches

--- docker/kolab/utils/23-patch-system.sh
+++ docker/kolab/utils/23-patch-system.sh
@@ -2,9 +2,10 @@
 
 PATCHPATH=$(pwd)/patches
 
-# Example for applying a pykolab patch
-#pushd /usr/lib/python2.7/site-packages/ || exit
-#patch -p1 < "$PATCHPATH/0001-Resolve-base_dn-in-kolab_user_base_dn-user_base_dn-a.patch"
-#popd || exit
-#systemctl restart kolabd
-#systemctl restart wallace
+pushd /usr/lib/python2.7/site-packages/ || exit
+patch -p1 < "$PATCHPATH/0001-Resolve-base_dn-in-kolab_user_base_dn-user_base_dn-a.patch"
+patch -p1 < "$PATCHPATH/0001-Make-iTip-messages-outlook-compatible.patch"
+patch -p1 < "$PATCHPATH/0002-Implement-ACT_STORE_AND_NOTIFY-policy-for-resources-.patch"
+popd || exit
+systemctl restart kolabd
+systemctl restart wallace
