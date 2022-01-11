590c572665d35d1a18755d1e96b71c4bff3f99e8
Christian Mollekopf <mollekopf@kolabsys.com>
2022-01-11 23:39:21 +0100

    Enable debugging for wallace as well.
    Summary:
    # Why is this change necessary?
    # How does it address the issue?
    # What side effects does this change have?
    #
    #
    #BUG: <bug number>
    #FIXED-IN: <optional release version>
    # --[ Add to release changelog optionally close wish in bugs.kde.org ]-|
    #FEATURE: <optional bug number>
    #FIXED-IN: <optional release version>
    # --[ Copy commit message to a bug or wish in bugs.kde.org ]-----------|
    #CCBUG: <bug number>
    #CCMAIL: <email>
    #REVIEW: <review number>
    # --[ Notify documentation team of user visible changes ]--------------|
    #GUI:
    # --[ Notify Commit Digest team of something interesting ]-------------|
    #DIGEST:
    #GIT_SILENT
    
    Differential Revision: https://git.kolab.org/D3274

--- docker/kolab/utils/09-enable-debugging.sh
+++ docker/kolab/utils/09-enable-debugging.sh
@@ -6,3 +6,6 @@ echo "debug: 1" >> /etc/imapd.conf
 systemctl restart cyrus-imapd
 
 sed -i -r -e "s/_debug'] = (.*);/_debug'] = true;/g" /etc/roundcubemail/config.inc.php
+
+echo "FLAGS=\"--fork -l debug -d 8\"" > /etc/sysconfig/wallace
+systemctl restart wallace
