bb3c2f6dfea30439950d52eaf4e7f83765d614d4
Christian Mollekopf <mollekopf@kolabsys.com>
2022-03-23 15:03:26 +0100

    deploy.sh script

---
+++ bin/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+bin/quickstart.sh --nodev
+
+docker exec -w /src/kolabsrc/ kolab-webapp ./artisan user:assign john@kolab.org meet
+docker exec -w /src/kolabsrc/ kolab-webapp ./artisan user:assign john@kolab.org beta
