cfa95a6626a7aba480da2c37f32b8b9bc19d0c1c
Aleksander Machniak <machniak@kolabsys.com>
2022-04-27 11:13:44 +0200

    Improve companion app icon

--- src/resources/vue/Dashboard.vue
+++ src/resources/vue/Dashboard.vue
@@ -4,46 +4,46 @@
 
         <div id="dashboard-nav">
             <router-link class="card link-profile" :to="{ name: 'profile' }">
-                <svg-icon icon="user-cog"></svg-icon><span class="name">{{ $t('dashboard.profile') }}</span>
+                <svg-icon icon="user-cog"></svg-icon><span>{{ $t('dashboard.profile') }}</span>
             </router-link>
             <router-link v-if="status.enableDomains" class="card link-domains" :to="{ name: 'domains' }">
-                <svg-icon icon="globe"></svg-icon><span class="name">{{ $t('dashboard.domains') }}</span>
+                <svg-icon icon="globe"></svg-icon><span>{{ $t('dashboard.domains') }}</span>
             </router-link>
             <router-link v-if="status.enableUsers" class="card link-users" :to="{ name: 'users' }">
-                <svg-icon icon="user-friends"></svg-icon><span class="name">{{ $t('dashboard.users') }}</span>
+                <svg-icon icon="user-friends"></svg-icon><span>{{ $t('dashboard.users') }}</span>
             </router-link>
             <router-link v-if="status.enableDistlists" class="card link-distlists" :to="{ name: 'distlists' }">
-                <svg-icon icon="users"></svg-icon><span class="name">{{ $t('dashboard.distlists') }}</span>
+                <svg-icon icon="users"></svg-icon><span>{{ $t('dashboard.distlists') }}</span>
                 <span class="badge bg-primary">{{ $t('dashboard.beta') }}</span>
             </router-link>
             <router-link v-if="status.enableResources" class="card link-resources" :to="{ name: 'resources' }">
-                <svg-icon icon="cog"></svg-icon><span class="name">{{ $t('dashboard.resources') }}</span>
+                <svg-icon icon="cog"></svg-icon><span>{{ $t('dashboard.resources') }}</span>
                 <span class="badge bg-primary">{{ $t('dashboard.beta') }}</span>
             </router-link>
             <router-link v-if="status.enableFolders" class="card link-shared-folders" :to="{ name: 'shared-folders' }">
-                <svg-icon icon="folder-open"></svg-icon><span class="name">{{ $t('dashboard.shared-folders') }}</span>
+                <svg-icon icon="folder-open"></svg-icon><span>{{ $t('dashboard.shared-folders') }}</span>
                 <span class="badge bg-primary">{{ $t('dashboard.beta') }}</span>
             </router-link>
             <router-link v-if="status.enableWallets" class="card link-wallet" :to="{ name: 'wallet' }">
-                <svg-icon icon="wallet"></svg-icon><span class="name">{{ $t('dashboard.wallet') }}</span>
+                <svg-icon icon="wallet"></svg-icon><span>{{ $t('dashboard.wallet') }}</span>
                 <span v-if="balance < 0" class="badge bg-danger">{{ $root.price(balance, currency) }}</span>
             </router-link>
             <router-link v-if="$root.hasSKU('meet') && !$root.isDegraded()" class="card link-chat" :to="{ name: 'rooms' }">
-                <svg-icon icon="comments"></svg-icon><span class="name">{{ $t('dashboard.chat') }}</span>
+                <svg-icon icon="comments"></svg-icon><span>{{ $t('dashboard.chat') }}</span>
                 <span class="badge bg-primary">{{ $t('dashboard.beta') }}</span>
             </router-link>
             <router-link v-if="status.enableFiles && !$root.isDegraded()" class="card link-files" :to="{ name: 'files' }">
-                <svg-icon icon="folder-open"></svg-icon><span class="name">{{ $t('dashboard.files') }}</span>
+                <svg-icon icon="folder-open"></svg-icon><span>{{ $t('dashboard.files') }}</span>
                 <span class="badge bg-primary">{{ $t('dashboard.beta') }}</span>
             </router-link>
             <router-link v-if="status.enableSettings" class="card link-settings" :to="{ name: 'settings' }">
-                <svg-icon icon="sliders-h"></svg-icon><span class="name">{{ $t('dashboard.settings') }}</span>
+                <svg-icon icon="sliders-h"></svg-icon><span>{{ $t('dashboard.settings') }}</span>
             </router-link>
             <a v-if="webmailURL" class="card link-webmail" :href="webmailURL">
-                <svg-icon icon="envelope"></svg-icon><span class="name">{{ $t('dashboard.webmail') }}</span>
+                <svg-icon icon="envelope"></svg-icon><span>{{ $t('dashboard.webmail') }}</span>
             </a>
             <router-link v-if="status.enableCompanionapps" class="card link-companionapp" :to="{ name: 'companion' }">
-                <svg-icon icon="mobile"></svg-icon><span class="name">{{ $t('dashboard.companion') }}</span>
+                <svg-icon icon="mobile-alt"></svg-icon><span>{{ $t('dashboard.companion') }}</span>
                 <span class="badge bg-primary">{{ $t('dashboard.beta') }}</span>
             </router-link>
         </div>
