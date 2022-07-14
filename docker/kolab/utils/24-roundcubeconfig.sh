2447382a82678190a4b0a293c8af6651d130c839
Christian Mollekopf <mollekopf@apheleia-it.ch>
2022-07-14 17:12:27 +0200

    Fixed sed replacements

--- docker/kolab/utils/24-roundcubeconfig.sh
+++ docker/kolab/utils/24-roundcubeconfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i -r -e "s/\$config['kolab_files_url'] =.*$/\$config['kolab_files_url'] = 'https://' \. \$_SERVER['HTTP_HOST'] . '/chwala/';/g" /etc/roundcubemail/kolab_files.inc.php
+sed -i -r -e "s|$config\['kolab_files_url'\] = .*$|$config['kolab_files_url'] = 'https://' \. \$_SERVER['HTTP_HOST'] . '/chwala/';|g" /etc/roundcubemail/kolab_files.inc.php
 
-sed -i -r -e "s/\$config['kolab_invitation_calendars'] =.*$/\$config['kolab_invitation_calendars'] = true;/g" /etc/roundcubemail/calendar.inc.php
+sed -i -r -e "s|$config\['kolab_invitation_calendars'\] = .*$|$config['kolab_invitation_calendars'] = true;|g" /etc/roundcubemail/calendar.inc.php
 
 sed -i -r -e "/^.*'contextmenu',$/a 'enigma'," /etc/roundcubemail/config.inc.php
