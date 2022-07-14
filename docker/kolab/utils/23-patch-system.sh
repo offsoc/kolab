a8db93408e434497762ef3a1a3446332c4f67354
Christian Mollekopf <mollekopf@apheleia-it.ch>
2022-07-14 13:36:04 +0200

    Dropped patches that are now packaged.

--- docker/kolab/utils/23-patch-system.sh
+++ docker/kolab/utils/23-patch-system.sh
@@ -2,10 +2,8 @@
 
 PATCHPATH=$(pwd)/patches
 
-pushd /usr/lib/python2.7/site-packages/ || exit
-patch -p1 < "$PATCHPATH/0001-Resolve-base_dn-in-kolab_user_base_dn-user_base_dn-a.patch"
-patch -p1 < "$PATCHPATH/0001-Make-iTip-messages-outlook-compatible.patch"
-patch -p1 < "$PATCHPATH/0002-Implement-ACT_STORE_AND_NOTIFY-policy-for-resources-.patch"
+pushd /usr/share/roundcubemail/ || exit
+# patch -p1 -l < "$PATCHPATH/0002-WOAT-support.patch"
+# patch -p1 -l < "$PATCHPATH/0003-PROXY-protocol-support.patch"
 popd || exit
-systemctl restart kolabd
-systemctl restart wallace
+# systemctl restart httpd
