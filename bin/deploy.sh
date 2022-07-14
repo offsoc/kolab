52422f835d7122ba16438d38a9ed75c0fca42b6a
Christian Mollekopf <mollekopf@apheleia-it.ch>
2022-07-14 13:12:50 +0200

    The meet SKU is no more

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 bin/quickstart.sh --nodev
 
-docker exec -w /src/kolabsrc/ kolab-webapp ./artisan user:assign john@kolab.org meet
 docker exec -w /src/kolabsrc/ kolab-webapp ./artisan user:assign john@kolab.org beta
