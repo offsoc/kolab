f52e4f78d64ed73db2bc2620fbd9c3d7f0a782eb
Christian Mollekopf <mollekopf@apheleia-it.ch>
2022-07-15 14:34:57 +0200

    Enable passwordless and woat and multihost (for WOAT)

--- docker/kolab/utils/24-roundcubeconfig.sh
+++ docker/kolab/utils/24-roundcubeconfig.sh
@@ -5,3 +5,9 @@ sed -i -r -e "s|$config\['kolab_files_url'\] = .*$|$config['kolab_files_url'] = 
 sed -i -r -e "s|$config\['kolab_invitation_calendars'\] = .*$|$config['kolab_invitation_calendars'] = true;|g" /etc/roundcubemail/calendar.inc.php
 
 sed -i -r -e "/^.*'contextmenu',$/a 'enigma'," /etc/roundcubemail/config.inc.php
+
+sed -i -r -e "s|$config\['enigma_passwordless'\] = .*$|$config['enigma_passwordless'] = true;|g" /etc/roundcubemail/enigma.inc.php
+sed -i -r -e "s|$config\['enigma_multihost'\] = .*$|$config['enigma_multihost'] = true;|g" /etc/roundcubemail/enigma.inc.php
+
+echo "\$config['enigma_woat'] = true;" >> /etc/roundcubemail/enigma.inc.php
+
