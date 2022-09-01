8db6e74a3bc8cbab109845039ea883761b76e195
Christian Mollekopf <mollekopf@apheleia-it.ch>
2022-09-01 13:52:46 +0200

    Revert "The patches are now in packaging"
    This reverts commit 87881547b1450ab4165c30d024ae582dc5fe10de.
    
    They are not in testing though

--- docker/kolab/utils/23-patch-system.sh
+++ docker/kolab/utils/23-patch-system.sh
@@ -3,7 +3,7 @@
 PATCHPATH=$(pwd)/patches
 
 pushd /usr/share/roundcubemail/ || exit
-# patch -p1 -l < "$PATCHPATH/0002-WOAT-support.patch"
-# patch -p1 -l < "$PATCHPATH/0003-PROXY-protocol-support.patch"
+patch -p1 -l < "$PATCHPATH/0002-WOAT-support.patch"
+patch -p1 -l < "$PATCHPATH/0003-PROXY-protocol-support.patch"
 popd || exit
-# systemctl restart httpd
+systemctl restart httpd
