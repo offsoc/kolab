b95e69240bd8fea24c30eaf3c88ea6032b4cc018
Christian Mollekopf <mollekopf@kolabsys.com>
2022-09-08 21:31:35 +0200

    Removed openvidu containers

--- docker/openvidu-dev/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-cp -R /src/openvidu /src/openvidusrc
-cd /src/openvidusrc
-mvn -DskipTests=true clean install
-/usr/bin/java -jar /src/openvidusrc/openvidu-server/target/openvidu-server-2.18.0.jar
