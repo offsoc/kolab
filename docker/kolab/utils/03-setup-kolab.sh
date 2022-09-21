df5e5a2dc38dc91a8edbb1d18ea54fbc497e0b5f
Christian Mollekopf <mollekopf@apheleia-it.ch>
2022-09-21 23:19:19 +0200

    Made the ldap setup optional

--- docker/kolab/utils/03-setup-kolab.sh
+++ docker/kolab/utils/03-setup-kolab.sh
@@ -11,34 +11,12 @@ while ! mysqladmin -u root ping > /dev/null 2>&1 ; do
 done | tee -a /root/setup-kolab.log
 echo "OK!" | tee -a /root/setup-kolab.log
 
-
-# if [ -f /root/kolab.conf.template ]; then
-#     eval "echo \"$(cat /root/kolab.conf.template)\"" > /root/kolab.conf.ref
-#     KOLAB_CONFIG_REF="--config=/root/kolab.conf.ref"
-#     cp -f ${KOLAB_CONFIG_REF#--config=} /etc/kolab/kolab.conf
-# fi
-
-if [ -d "/var/lib/dirsrv/slapd-kolab/" ]; then
-    echo "LDAP directory exists"
-    #FIXME not implemented
-    exit 1
-else
-    echo "LDAP directory does not exist"
-    CMD="$(which setup-kolab) ldap \
-        --default ${LDAP_HOST} \
-        --fqdn=kolab.${domain}  \
-        --directory-manager-pwd=${LDAP_ADMIN_BIND_PW:-Welcome2KolabSystems}"
-    ${CMD} 2>&1 | tee -a /root/setup-kolab.log
-fi
-
-if [ ! -z "${LDAP_HOST}" ]; then
-    echo -n "Wait for DS389 container: " | tee -a /root/setup-kolab.log
-    while ! ldapsearch -h ${LDAP_HOST} -D "${LDAP_ADMIN_BIND_DN}" -w "${LDAP_ADMIN_BIND_PW}" -b "" -s base > /dev/null 2>&1 ; do
-            echo -n '.'
-            sleep 3
-    done | tee -a /root/setup-kolab.log
-    echo "OK!" | tee -a /root/setup-kolab.log
-fi
+echo -n "Wait for DS389 container: " | tee -a /root/setup-kolab.log
+while ! ldapsearch -h ${LDAP_HOST} -D "${LDAP_ADMIN_BIND_DN}" -w "${LDAP_ADMIN_BIND_PW}" -b "" -s base > /dev/null 2>&1 ; do
+        echo -n '.'
+        sleep 3
+done | tee -a /root/setup-kolab.log
+echo "OK!" | tee -a /root/setup-kolab.log
 
 
 cat > /tmp/kolab-setup-my.cnf << EOF
