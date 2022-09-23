78c41604c5504441c0578bdeda19f0ede54357cc
Christian Mollekopf <mollekopf@apheleia-it.ch>
2022-09-23 21:50:19 +0200

    Point imap and smtp to the kolab container

--- src/config/imap.php
+++ src/config/imap.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'uri' => env('IMAP_URI', '127.0.0.1'),
+    'uri' => env('IMAP_URI', 'ssl://kolab:11993'),
     'admin_login' => env('IMAP_ADMIN_LOGIN', 'cyrus-admin'),
     'admin_password' => env('IMAP_ADMIN_PASSWORD', null),
     'verify_peer' => env('IMAP_VERIFY_PEER', true),
     'verify_host' => env('IMAP_VERIFY_HOST', true),
-    'host' => env('IMAP_HOST', '127.0.0.1'),
+    'host' => env('IMAP_HOST', 'kolab'),
     'imap_port' => env('IMAP_PORT', 12143),
     'guam_port' => env('IMAP_GUAM_PORT', 9143),
 ];
