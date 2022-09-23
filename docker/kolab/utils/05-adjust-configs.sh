7a43657fe5d5b2b7830ce5c2811aef353e688343
Christian Mollekopf <mollekopf@apheleia-it.ch>
2022-09-23 09:52:37 +0200

    Networking fixes

--- docker/kolab/utils/05-adjust-configs.sh
+++ docker/kolab/utils/05-adjust-configs.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
-# if [[ ${DB_HOST} == "localhost" || ${DB_HOST} == "127.0.0.1" ]]; then
-#     mysql -h ${DB_HOST:-127.0.0.1} -u root --password=${DB_ROOT_PASSWORD:-Welcome2KolabSystems} \
-#         -e "UPDATE mysql.db SET Host = '127.0.0.1' WHERE Host = 'localhost';"
-    
-#     mysql -h ${DB_HOST:-127.0.0.1} -u root --password=${DB_ROOT_PASSWORD:-Welcome2KolabSystems} \
-#         -e "FLUSH PRIVILEGES;"
-# fi
-
 # Replace localhost
-sed -i -e "/hosts/s/localhost/${LDAP_HOST:-127.0.0.1}/" /etc/iRony/dav.inc.php
-sed -i -e "/host/s/localhost/${LDAP_HOST:-127.0.0.1}/g" \
-       -e "/fbsource/s/localhost/${IMAP_HOST:-127.0.0.1}/g" /etc/kolab-freebusy/config.ini
-#sed -i -e "s/server_host.*/server_host = ${LDAP_HOST:-127.0.0.1}/g" /etc/postfix/ldap/*
-sed -i -e "/password_ldap_host/s/localhost/${LDAP_HOST:-127.0.0.1}/" /etc/roundcubemail/password.inc.php
-sed -i -e "/hosts/s/localhost/${LDAP_HOST:-127.0.0.1}/" /etc/roundcubemail/kolab_auth.inc.php
+sed -i -e "/hosts/s/localhost/${LDAP_HOST}/" /etc/iRony/dav.inc.php
+sed -i -e "/host/s/localhost/${LDAP_HOST}/g" \
+       -e "/fbsource/s/localhost/${IMAP_HOST}/g" /etc/kolab-freebusy/config.ini
+#sed -i -e "s/server_host.*/server_host = ${LDAP_HOST}/g" /etc/postfix/ldap/*
+sed -i -e "/password_ldap_host/s/localhost/${LDAP_HOST}/" /etc/roundcubemail/password.inc.php
+sed -i -e "/hosts/s/localhost/${LDAP_HOST}/" /etc/roundcubemail/kolab_auth.inc.php
 sed -i -e "s#.*db_dsnw.*#    \$config['db_dsnw'] = 'mysql://${DB_RC_USERNAME}:${DB_RC_PASSWORD}@${DB_HOST}/roundcube';#" \
-       -e "/default_host/s|= .*$|= 'ssl://${IMAP_HOST:-127.0.0.1}';|" \
-       -e "/default_port/s|= .*$|= ${IMAP_PORT:-11993};|" \
-       -e "/smtp_server/s|= .*$|= 'tls://${MAIL_HOST:-127.0.0.1}';|" \
-       -e "/smtp_port/s/= .*$/= ${MAIL_PORT:-10587};/" \
-       -e "/hosts/s/localhost/${LDAP_HOST:-127.0.0.1}/" /etc/roundcubemail/config.inc.php
-sed -i -e "/hosts/s/localhost/${LDAP_HOST:-127.0.0.1}/" /etc/roundcubemail/calendar.inc.php
+       -e "/default_host/s|= .*$|= 'ssl://${IMAP_HOST}';|" \
+       -e "/default_port/s|= .*$|= ${IMAP_PORT};|" \
+       -e "/smtp_server/s|= .*$|= 'tls://${MAIL_HOST}';|" \
+       -e "/smtp_port/s/= .*$/= ${MAIL_PORT};/" \
+       -e "/hosts/s/localhost/${LDAP_HOST}/" /etc/roundcubemail/config.inc.php
+sed -i -e "/hosts/s/localhost/${LDAP_HOST}/" /etc/roundcubemail/calendar.inc.php
 
 
 . ./settings.sh
@@ -152,7 +144,7 @@ sed -i -r -e "s|$config\['enigma_multihost'\] = .*$|$config['enigma_multihost'] 
 echo "\$config['enigma_woat'] = true;" >> /etc/roundcubemail/enigma.inc.php
 
 # Run it over haproxy then nginx for 2fa. We need to use startls because otherwise the proxy protocol doesn't work.
-sed -i -r -e "s|$config\['default_host'\] = .*$|$config['default_host'] = 'tls://127.0.0.1';|g" /etc/roundcubemail/config.inc.php
+sed -i -r -e "s|$config\['default_host'\] = .*$|$config['default_host'] = 'tls://haproxy';|g" /etc/roundcubemail/config.inc.php
 sed -i -r -e "s|$config\['default_port'\] = .*$|$config['default_port'] = 145;|g" /etc/roundcubemail/config.inc.php
 
 # So we can just append
@@ -172,8 +164,3 @@ cat << EOF >> /etc/roundcubemail/config.inc.php
 EOF
 
 echo "?>" >> /etc/roundcubemail/config.inc.php
-
-
-# Send dns queries over powerdns
-# rm -f /etc/resolv.conf
-# echo "nameserver 127.0.0.1:9953" > /etc/resolv.conf
