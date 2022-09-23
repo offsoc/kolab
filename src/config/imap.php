d807448a2e9227f6e76100241bf86882305565bd
Christian Mollekopf <mollekopf@apheleia-it.ch>
2022-09-23 23:12:56 +0200

    The nginx proxy needs an ip
    alternatively we could resolve the hostname in the controller I suppose

--- src/config/imap.php
+++ src/config/imap.php
@@ -6,7 +6,7 @@ return [
     'admin_password' => env('IMAP_ADMIN_PASSWORD', null),
     'verify_peer' => env('IMAP_VERIFY_PEER', true),
     'verify_host' => env('IMAP_VERIFY_HOST', true),
-    'host' => env('IMAP_HOST', 'kolab'),
+    'host' => env('IMAP_HOST', '172.18.0.5'),
     'imap_port' => env('IMAP_PORT', 12143),
     'guam_port' => env('IMAP_GUAM_PORT', 9143),
 ];
