c31628b8bc62aea389853e0d9e37ff58103df353
Christian Mollekopf <mollekopf@apheleia-it.ch>
2022-12-12 23:06:07 +0100

    Cleanup

--- src/app/Http/Controllers/API/V4/NGINXController.php
+++ src/app/Http/Controllers/API/V4/NGINXController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\API\V4;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class NGINXController extends Controller
 {
