c99e8be93682770d130ddcf6c3f62d3f407cbfc1
Jeroen van Meeuwen (Apheleia IT) <vanmeeuwen@apheleia-it.ch>
2023-02-01 14:22:36 +0100

    composer fails if the directory for database/seeds doesn't exist, since it is included in the class map appandage

---
+++ src/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Database\Seeds;
+
+// phpcs:ignore
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    }
+}
