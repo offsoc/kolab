44f55e48f99ecca2013d470d8d57a8ffef5b6340
Aleksander Machniak <machniak@kolabsys.com>
2023-03-08 10:34:44 +0100

    Support theme's fonts dir, fix fatal error in localization

--- src/resources/build/before.php
+++ src/resources/build/before.php
@@ -68,14 +68,19 @@ foreach (glob("{$rootDir}/resources/themes/*", GLOB_ONLYDIR) as $file) {
         mkdir("{$rootDir}/public/themes/{$theme}");
     }
 
-    if (!file_exists("{$rootDir}/public/themes/{$theme}/images")) {
-        mkdir("{$rootDir}/public/themes/{$theme}/images");
-    }
+    // TODO: Public dirs (glob patterns) should be in theme's meta.json
 
-    foreach (glob("{$file}/images/*") as $file) {
-        $path = explode('/', $file);
-        $image = $path[count($path)-1];
-        copy($file, "{$rootDir}/public/themes/{$theme}/images/{$image}");
+    foreach (['images', 'fonts'] as $subDir) {
+        if (file_exists("{$rootDir}/resources/themes/{$theme}/{$subDir}")) {
+            if (!file_exists("{$rootDir}/public/themes/{$theme}/{$subDir}")) {
+                mkdir("{$rootDir}/public/themes/{$theme}/{$subDir}");
+            }
+
+            foreach (glob("{$rootDir}/resources/themes/{$theme}/{$subDir}/*") as $file) {
+                $filename = pathinfo($file, PATHINFO_BASENAME);
+                copy($file, "{$rootDir}/public/themes/{$theme}/{$subDir}/{$filename}");
+            }
+        }
     }
 }
 
