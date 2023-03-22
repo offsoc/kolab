72879e9fc623e7952b29008e1de33281ebfdded1
Christian Mollekopf <mollekopf@apheleia-it.ch>
2023-03-22 21:22:19 +0100

    Fixed running browsertests
    For non-browser tests we don't need to run octane at all.
    For browser tests we need to make sure we connect without https to the
    local octane instance, so we test the code inside the container.

--- docker/tests/init.sh
+++ docker/tests/init.sh
@@ -34,8 +34,6 @@ fi
 
 npm run dev
 
-# /usr/bin/chromium-browser --no-sandbox --headless --disable-gpu --remote-debugging-port=9222 http://localhost &
-
 rm -rf database/database.sqlite
 ./artisan db:ping --wait
 php -dmemory_limit=512M ./artisan migrate --force
@@ -44,8 +42,6 @@ if test "$( env APP_DEBUG=false ./artisan -n users | wc -l )" -lt "1"; then
 fi
 ./artisan data:import || :
 ./artisan queue:work --stop-when-empty
-./artisan octane:start --host=$(grep OCTANE_HTTP_HOST .env | tail -n1 | sed "s/OCTANE_HTTP_HOST=//") >/dev/null 2>&1 &
-
 
 
 if [ "$1" == "suite-Functional" ]; then
@@ -72,6 +68,18 @@ if [ "$1" == "suite-Feature" ]; then
         --testsuite Feature
 fi
 if [ "$1" == "suite-Browser" ]; then
+
+    # Can't do browser tests over https
+    echo "APP_URL=http://kolab.local" >> .env
+    echo "APP_PUBLIC_URL=http://kolab.local" >> .env
+    echo "ASSET_URL=http://kolab.local" >> .env
+    echo "MEET_SERVER_URLS=http://kolab.local/meetmedia/api/" >> .env
+    echo "APP_HEADER_CSP=" >> .env
+    echo "APP_HEADER_XFO=" >> .env
+
+    ./artisan octane:start --port=80 >/dev/null 2>&1 &
+    echo "127.0.0.1 kolab.local admin.kolab.local reseller.kolab.local" >> /etc/hosts
+
     php \
         -dmemory_limit=-1 \
         vendor/bin/phpunit \
