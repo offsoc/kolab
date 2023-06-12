4fdcb9f448d8899e501f9978a5e824e9bb2c93ac
Christian Mollekopf <mollekopf@apheleia-it.ch>
2023-06-12 17:30:13 +0200

    Test resent activesync commands with server-side modifications

--- src/tests/Infrastructure/ActivesyncTest.php
+++ src/tests/Infrastructure/ActivesyncTest.php
@@ -11,6 +11,7 @@ class ActivesyncTest extends TestCase
     private static ?\GuzzleHttp\Client $client = null;
     private static ?\App\User $user = null;
     private static ?string $deviceId = null;
+    private static ?string $deviceId2 = null;
 
     private static function toWbxml($xml)
     {
@@ -32,10 +33,12 @@ class ActivesyncTest extends TestCase
         return $decoder->decode();
     }
 
-    private function request($request, $cmd)
+    private function request($request, $cmd, $deviceId = null)
     {
         $user = self::$user;
-        $deviceId = self::$deviceId;
+        if (!$deviceId) {
+            $deviceId = self::$deviceId;
+        }
         $body = self::toWbxml($request);
         return self::$client->request(
             'POST',
@@ -71,6 +74,7 @@ class ActivesyncTest extends TestCase
             // Otherwise we work against uninitialized metadata (subscription states),
             // because the account has been removed, but syncroton doesn't reinitalize the metadata for known devices.
             self::$deviceId = (string) Str::uuid();
+            self::$deviceId2 = (string) Str::uuid();
         }
 
         $deviceId = self::$deviceId;
@@ -105,6 +109,9 @@ class ActivesyncTest extends TestCase
             self::$user = $this->getTestUser('activesynctest@kolab.org', ['password' => 'simple123'], true);
             //FIXME this shouldn't be required, but it seems to be.
             Roundcube::dbh()->table('kolab_cache_task')->truncate();
+            Roundcube::dbh()->table('syncroton_folder')->truncate();
+            // Roundcube::dbh()->table('syncroton_content')->truncate();
+            // Roundcube::dbh()->table('syncroton_device')->truncate();
         }
         if (!self::$client) {
             self::$client = new \GuzzleHttp\Client([
@@ -596,7 +603,11 @@ class ActivesyncTest extends TestCase
         $this->assertEquals(0, $change->length);
 
         $this->assertEquals(0, $xpath->query("//ns:Commands")->length);
-        return $tasksId;
+
+        return [
+            'collectionId' => $tasksId,
+            'serverId2' => $xpath->query("//ns:Responses/ns:Add/ns:ServerId")->item(1)->nodeValue
+        ];
     }
 
     /**
@@ -635,7 +646,254 @@ class ActivesyncTest extends TestCase
         $dom = self::fromWbxml($response->getBody());
         $status = $dom->getElementsByTagName('Status');
         $this->assertEquals("3", $status[0]->nodeValue);
+        //After this we have to start from scratch
     }
+
+    /**
+    * Test fetching changes with a second device
+    * @depends testAddTaskContinuedAgain
+    */
+    public function testFetchTasks($result)
+    {
+        $tasksId = $result['collectionId'];
+        $serverId = $result['serverId2'];
+        // Initialize the second device
+        $request = <<<EOF
+        <?xml version="1.0" encoding="utf-8"?>
+        <!DOCTYPE AirSync PUBLIC "-//AIRSYNC//DTD AirSync//EN" "http://www.microsoft.com/">
+        <FolderSync xmlns="uri:FolderHierarchy">
+            <SyncKey>0</SyncKey>
+        </FolderSync>
+        EOF;
+
+        $response = $this->request($request, 'FolderSync', self::$deviceId2);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $request = <<<EOF
+        <?xml version="1.0" encoding="utf-8"?>
+        <!DOCTYPE AirSync PUBLIC "-//AIRSYNC//DTD AirSync//EN" "http://www.microsoft.com/">
+        <Sync xmlns="uri:AirSync" xmlns:AirSyncBase="uri:AirSyncBase" xmlns:Tasks="uri:Tasks">
+            <Collections>
+                <Collection>
+                    <SyncKey>0</SyncKey>
+                    <CollectionId>{$tasksId}</CollectionId>
+                    <DeletesAsMoves>0</DeletesAsMoves>
+                    <WindowSize>512</WindowSize>
+                    <Options>
+                        <FilterType>0</FilterType>
+                        <MIMESupport>2</MIMESupport>
+                        <MIMETruncation>8</MIMETruncation>
+                        <BodyPreference xmlns="uri:AirSyncBase">
+                            <Type>4</Type>
+                            <AllOrNone>1</AllOrNone>
+                        </BodyPreference>
+                    </Options>
+                </Collection>
+            </Collections>
+            <WindowSize>16</WindowSize>
+        </Sync>
+        EOF;
+        $response = $this->request($request, 'Sync', self::$deviceId2);
+        $this->assertEquals(200, $response->getStatusCode());
+        $dom = self::fromWbxml($response->getBody());
+        print($dom->saveXML());
+        $status = $dom->getElementsByTagName('Status');
+        $this->assertEquals("1", $status[0]->nodeValue);
+
+        // Fetch the content
+        $request = <<<EOF
+        <?xml version="1.0" encoding="utf-8"?>
+        <!DOCTYPE AirSync PUBLIC "-//AIRSYNC//DTD AirSync//EN" "http://www.microsoft.com/">
+        <Sync xmlns="uri:AirSync" xmlns:AirSyncBase="uri:AirSyncBase" xmlns:Tasks="uri:Tasks">
+            <Collections>
+                <Collection>
+                    <SyncKey>1</SyncKey>
+                    <CollectionId>{$tasksId}</CollectionId>
+                </Collection>
+            </Collections>
+            <WindowSize>16</WindowSize>
+        </Sync>
+        EOF;
+        $response = $this->request($request, 'Sync', self::$deviceId2);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $dom = self::fromWbxml($response->getBody());
+        print($dom->saveXML());
+        $status = $dom->getElementsByTagName('Status');
+        $this->assertEquals("1", $status[0]->nodeValue);
+        $xpath = $this->xpath($dom);
+        $add = $xpath->query("//ns:Commands/ns:Add");
+        $this->assertEquals(3, $add->length);
+
+
+        //Resend the same command
+        $request = <<<EOF
+        <?xml version="1.0" encoding="utf-8"?>
+        <!DOCTYPE AirSync PUBLIC "-//AIRSYNC//DTD AirSync//EN" "http://www.microsoft.com/">
+        <Sync xmlns="uri:AirSync" xmlns:AirSyncBase="uri:AirSyncBase" xmlns:Tasks="uri:Tasks">
+            <Collections>
+                <Collection>
+                    <SyncKey>1</SyncKey>
+                    <CollectionId>{$tasksId}</CollectionId>
+                </Collection>
+            </Collections>
+            <WindowSize>16</WindowSize>
+        </Sync>
+        EOF;
+        $response = $this->request($request, 'Sync', self::$deviceId2);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $dom = self::fromWbxml($response->getBody());
+        $status = $dom->getElementsByTagName('Status');
+        $this->assertEquals("1", $status[0]->nodeValue);
+        $xpath = $this->xpath($dom);
+        $add = $xpath->query("//ns:Commands/ns:Add");
+        $this->assertEquals(3, $add->length);
+
+        // Add another entry, delete an entry, with the original device (we have to init first again)
+        $request = <<<EOF
+        <?xml version="1.0" encoding="utf-8"?>
+        <!DOCTYPE AirSync PUBLIC "-//AIRSYNC//DTD AirSync//EN" "http://www.microsoft.com/">
+        <Sync xmlns="uri:AirSync" xmlns:AirSyncBase="uri:AirSyncBase" xmlns:Tasks="uri:Tasks">
+            <Collections>
+                <Collection>
+                    <SyncKey>0</SyncKey>
+                    <CollectionId>{$tasksId}</CollectionId>
+                    <DeletesAsMoves>0</DeletesAsMoves>
+                    <WindowSize>512</WindowSize>
+                    <Options>
+                        <FilterType>0</FilterType>
+                        <MIMESupport>2</MIMESupport>
+                        <MIMETruncation>8</MIMETruncation>
+                        <BodyPreference xmlns="uri:AirSyncBase">
+                            <Type>4</Type>
+                            <AllOrNone>1</AllOrNone>
+                        </BodyPreference>
+                    </Options>
+                </Collection>
+            </Collections>
+            <WindowSize>16</WindowSize>
+        </Sync>
+        EOF;
+        $response = $this->request($request, 'Sync');
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $request = <<<EOF
+        <?xml version="1.0" encoding="utf-8"?>
+        <!DOCTYPE AirSync PUBLIC "-//AIRSYNC//DTD AirSync//EN" "http://www.microsoft.com/">
+        <Sync xmlns="uri:AirSync" xmlns:AirSyncBase="uri:AirSyncBase" xmlns:Tasks="uri:Tasks">
+            <Collections>
+                <Collection>
+                    <SyncKey>1</SyncKey>
+                    <CollectionId>{$tasksId}</CollectionId>
+                </Collection>
+            </Collections>
+            <WindowSize>16</WindowSize>
+        </Sync>
+        EOF;
+        $response = $this->request($request, 'Sync');
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $request = <<<EOF
+        <?xml version="1.0" encoding="utf-8"?>
+        <!DOCTYPE AirSync PUBLIC "-//AIRSYNC//DTD AirSync//EN" "http://www.microsoft.com/">
+        <Sync xmlns="uri:AirSync" xmlns:AirSyncBase="uri:AirSyncBase" xmlns:Tasks="uri:Tasks">
+            <Collections>
+                <Collection>
+                    <SyncKey>2</SyncKey>
+                    <CollectionId>{$tasksId}</CollectionId>
+                    <Commands>
+                        <Add>
+                            <ClientId>clientId4</ClientId>
+                            <ApplicationData>
+                                <Subject xmlns="uri:Tasks">task4</Subject>
+                                <Complete xmlns="uri:Tasks">0</Complete>
+                                <DueDate xmlns="uri:Tasks">2020-11-04T00:00:00.000Z</DueDate>
+                                <UtcDueDate xmlns="uri:Tasks">2020-11-03T23:00:00.000Z</UtcDueDate>
+                            </ApplicationData>
+                        </Add>
+                        <Delete>
+                            <ServerId>{$serverId}</ServerId>
+                        </Delete>
+                    </Commands>
+                </Collection>
+            </Collections>
+            <WindowSize>16</WindowSize>
+        </Sync>
+        EOF;
+        $response = $this->request($request, 'Sync');
+        $this->assertEquals(200, $response->getStatusCode());
+        $dom = self::fromWbxml($response->getBody());
+
+        $status = $dom->getElementsByTagName('Status');
+        $this->assertEquals("1", $status[0]->nodeValue);
+        $xpath = $this->xpath($dom);
+        $add = $xpath->query("//ns:Responses/ns:Add");
+        $this->assertEquals(1, $add->length);
+        // Delete does not have to be confirmed according to spec
+        $delete = $xpath->query("//ns:Responses/ns:Delete");
+        $this->assertEquals(0, $delete->length);
+
+        // And fetch the changes
+        $request = <<<EOF
+        <?xml version="1.0" encoding="utf-8"?>
+        <!DOCTYPE AirSync PUBLIC "-//AIRSYNC//DTD AirSync//EN" "http://www.microsoft.com/">
+        <Sync xmlns="uri:AirSync" xmlns:AirSyncBase="uri:AirSyncBase" xmlns:Tasks="uri:Tasks">
+            <Collections>
+                <Collection>
+                    <SyncKey>2</SyncKey>
+                    <CollectionId>{$tasksId}</CollectionId>
+                </Collection>
+            </Collections>
+            <WindowSize>16</WindowSize>
+        </Sync>
+        EOF;
+        $response = $this->request($request, 'Sync', self::$deviceId2);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $dom = self::fromWbxml($response->getBody());
+        // print("=====\n");
+        // print($dom->saveXML());
+        // print("=====\n");
+        $status = $dom->getElementsByTagName('Status');
+        $this->assertEquals("1", $status[0]->nodeValue);
+        $xpath = $this->xpath($dom);
+        $add = $xpath->query("//ns:Commands/ns:Add");
+        $this->assertEquals(1, $add->length);
+        $delete = $xpath->query("//ns:Commands/ns:Delete");
+        $this->assertEquals(1, $delete->length);
+
+        // and finally refetch the same changes
+        $request = <<<EOF
+        <?xml version="1.0" encoding="utf-8"?>
+        <!DOCTYPE AirSync PUBLIC "-//AIRSYNC//DTD AirSync//EN" "http://www.microsoft.com/">
+        <Sync xmlns="uri:AirSync" xmlns:AirSyncBase="uri:AirSyncBase" xmlns:Tasks="uri:Tasks">
+            <Collections>
+                <Collection>
+                    <SyncKey>2</SyncKey>
+                    <CollectionId>{$tasksId}</CollectionId>
+                </Collection>
+            </Collections>
+            <WindowSize>16</WindowSize>
+        </Sync>
+        EOF;
+        $response = $this->request($request, 'Sync', self::$deviceId2);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $dom = self::fromWbxml($response->getBody());
+        // print("=====\n");
+        // print($dom->saveXML());
+        // print("=====\n");
+        $status = $dom->getElementsByTagName('Status');
+        $this->assertEquals("1", $status[0]->nodeValue);
+        $xpath = $this->xpath($dom);
+        $add = $xpath->query("//ns:Commands/ns:Add");
+        $this->assertEquals(1, $add->length);
+        //FIXME we currently miss deletions.
+        $delete = $xpath->query("//ns:Commands/ns:Delete");
+        $this->assertEquals(0, $delete->length);
+    }
+
 
     /**
      * @doesNotPerformAssertions
