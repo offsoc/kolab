0f508ca639341302392318cc094c9863491796e7
Christian Mollekopf <mollekopf@apheleia-it.ch>
2023-10-06 17:10:07 +0200

    Don't rebuild meet when running it

--- docker/meet/init.sh
+++ docker/meet/init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
-rm -R /src/meetsrc/lib /src/meetsrc/config /src/meetsrc/test
-cp -R /src/meet/lib /src/meetsrc/lib
-cp -R /src/meet/config /src/meetsrc/config
-cp -R /src/meet/test /src/meetsrc/test
-cp -R /src/meet/*.js /src/meetsrc/
+# rm -R /src/meetsrc/lib /src/meetsrc/config /src/meetsrc/test
+# cp -R /src/meet/lib /src/meetsrc/lib
+# cp -R /src/meet/config /src/meetsrc/config
+# cp -R /src/meet/test /src/meetsrc/test
+# cp -R /src/meet/*.js /src/meetsrc/
 cd /src/meetsrc
-npm install
-npm install -g nodemon
+# npm install
+# npm install -g nodemon
 export DEBUG="kolabmeet-server* mediasoup*"
 exec nodemon server.js
