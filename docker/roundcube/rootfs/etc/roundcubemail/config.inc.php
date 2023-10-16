48e496b2a3f076c4c89096f44f52b52f68f830e5
Christian Mollekopf <mollekopf@apheleia-it.ch>
2023-10-16 22:02:12 +0200

    Add fpm config to log to stdout
    in fpm.conf:
    [global]
    error_log = /proc/self/fd/2
    log_level = error
    log_buffering = no
    
    in www.conf:
    catch_workers_output = yes
    decorate_workers_output = no
    php_flag[display_errors] = off
    php_admin_flag[log_errors] = on
    php_admin_flag[fastcgi.logging] = off
    
    Inspired by https://phil.lavin.me.uk/2023/02/php-fpm-log-errors-to-stderr-for-docker/

--- docker/roundcube/rootfs/etc/roundcubemail/config.inc.php
+++ docker/roundcube/rootfs/etc/roundcubemail/config.inc.php
@@ -156,7 +156,7 @@
 
     $config['debug_level'] = 1;
     $config['devel_mode'] = false;
-    $config['log_driver'] = 'file';
+    $config['log_driver'] = 'stdout';
     $config['per_user_logging'] = true;
     $config['log_date_format'] = 'd-M-Y H:i:s,u O';
     $config['syslog_id'] = 'roundcube';
