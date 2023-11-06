30b0855b112a632fc3c37e23482315649f8ed61b
Christian Mollekopf <mollekopf@apheleia-it.ch>
2023-11-06 15:24:59 +0100

    Hook up sieve

--- docker/roundcube/rootfs/opt/app-root/src/roundcubemail-config-templates/managesieve.inc.php
+++ docker/roundcube/rootfs/opt/app-root/src/roundcubemail-config-templates/managesieve.inc.php
@@ -1,10 +1,10 @@
 <?php
     $config['managesieve_port'] = 4190;
-    $config['managesieve_host'] = '%h';
+    $config['managesieve_host'] = 'imap';
     $config['managesieve_auth_type'] = 'PLAIN';
     $config['managesieve_auth_cid'] = null;
     $config['managesieve_auth_pw'] = null;
-    $config['managesieve_usetls'] = true;
+    $config['managesieve_usetls'] = false;
     $config['managesieve_default'] = '/etc/dovecot/sieve/global';
     $config['managesieve_mbox_encoding'] = 'UTF-8';
     $config['managesieve_replace_delimiter'] = '';
@@ -14,15 +14,6 @@
 
     $config['managesieve_filename_extension'] = '';
     $config['managesieve_kolab_master'] = true;
-
-    // ManageSieve Connection TLS settings, adjust for Production
-    $config['managesieve_conn_options'] = Array(
-            'ssl' => Array(
-                    'verify_peer_name' => false,
-                    'verify_peer' => false,
-                    'allow_self_signed' => true
-                )
-        );
 
     if (file_exists(RCUBE_CONFIG_DIR . '/' . $_SERVER["HTTP_HOST"] . '/' . basename(__FILE__))) {
         include_once(RCUBE_CONFIG_DIR . '/' . $_SERVER["HTTP_HOST"] . '/' . basename(__FILE__));
