c9882254f4f3d0ba6091de231f5f9c6b162de2f2
Christian Mollekopf <mollekopf@apheleia-it.ch>
2023-11-14 11:58:19 +0100

    cyrus-imap murder test scripts

--- config.docker-dev/testimap.sh
+++ config.docker-dev/testimap.sh
@@ -8,8 +8,9 @@ ADMIN_USER="cyrus-admin"
 ADMIN_PASSWORD="Welcome2KolabSystems"
 USER="admin@kolab.local"
 PASSWORD="simple123"
-CREDENTIALS="$USER:$PASSWORD"
 
+USER_FRONTEND_COMMAND="imtest -p 7143 -a $USER -u $USER -w $PASSWORD -v 127.0.0.1"
+USER_BACKEND_COMMAND="imtest -p 8143 -a $USER -u $USER -w $PASSWORD -v 127.0.0.1"
 ADMIN_FRONTEND_COMMAND="imtest -p 7143 -a $ADMIN_USER -u $ADMIN_USER -w $ADMIN_PASSWORD -v 127.0.0.1"
 ADMIN_BACKEND1_COMMAND="imtest -p 8143 -a $ADMIN_USER -u $ADMIN_USER -w $ADMIN_PASSWORD -v 127.0.0.1"
 ADMIN_BACKEND2_COMMAND="imtest -p 9143 -a $ADMIN_USER -u $ADMIN_USER -w $ADMIN_PASSWORD -v 127.0.0.1"
@@ -33,24 +34,29 @@ echo 'a01 list "" "*"' | $ADMIN_FRONTEND_COMMAND | grep "shared/test3@kolab.loca
 echo 'a01 list "" "*"' | $ADMIN_FRONTEND_COMMAND | grep "shared/test4@kolab.local"
 echo 'a01 list "" "*"' | $ADMIN_FRONTEND_COMMAND | grep "shared/test5@kolab.local"
 
-# echo 'a01 list "" "*"' | imtest -p 7143 -a $USER -u $USER -w $PASSWORD -v 127.0.0.1
-# echo 'a01 list "" "*"' | $ADMIN_FRONTEND_COMMAND
+
+# Test getmetadata/setmetadata on a shared folder
+echo 'a01 setmetadata shared/test1@kolab.local (/shared/vendor/kolab/folder-type "event")' | $ADMIN_FRONTEND_COMMAND | grep "a01 OK Completed"
+echo 'a01 getmetadata (DEPTH infinity) shared/test1@kolab.local (/shared/vendor/kolab/folder-type)' | $ADMIN_FRONTEND_COMMAND | grep 'METADATA shared/test1@kolab.local ("/shared/vendor/kolab/folder-type" "event")'> /dev/null 2>&1
+# Also test requesting multiple items
+echo 'a01 getmetadata (DEPTH infinity) shared/test1@kolab.local (/shared/vendor/kolab/folder-type /private/vendor/kolab/folder-type)' | $ADMIN_FRONTEND_COMMAND | grep 'METADATA shared/test1@kolab.local ("/shared/vendor/kolab/folder-type" "event" "/private/vendor/kolab/folder-type" NIL)'> /dev/null 2>&1
 
 
-# echo 'a01 list "" "*"' | $ADMIN_FRONTEND_COMMAND
-# docker compose exec -ti imap-mupdate ctl_mboxlist -d | grep '"shared":'
-# echo 'a01 setacl shared anyone +x' | $ADMIN_FRONTEND_COMMAND  > /dev/null 2>&1
-# echo 'a01 delete shared' | $ADMIN_FRONTEND_COMMAND > /dev/null 2>&1
-# echo 'a01 setacl shared/test4@kolab.local anyone +x' | $ADMIN_FRONTEND_COMMAND > /dev/null 2>&1
-# echo 'a01 delete shared/test4@kolab.local' | $ADMIN_FRONTEND_COMMAND > /dev/null 2>&1
-# # docker compose exec -ti imap-mupdate ctl_mboxlist -d
 
-# # cyradm --user cyrus-admin --password $(awk '/mupdate_password/ {print $2}' /etc/imapd.conf)  --authz cyrus-admin localhost
+# docker compose exec -ti webapp ./artisan scalpel:user:create --id=3 --email=john@kolab.local --password=simple123 --status=3
+# $USER_FRONTEND_COMMAND
+# We must login via IMAP first, to create the INBOX.
+echo 'a01 list "" "*"' | $USER_BACKEND_COMMAND > /dev/null 2>&1
+# Ensure the inbox is created
+docker compose exec -ti imap-mupdate ctl_mboxlist -d | grep '"kolab.local!user.admin":' > /dev/null 2>&1
 
-# # curl -u $CREDENTIALS -i -X PROPFIND -H 'Depth: 1' $DAVSERVER/principals/user/$USER/
-
-# # curl --user "$CREDENTIALS" -sD /dev/stderr -H "Content-Type: application/xml" -X PROPFIND -H "Depth: infinity" --data '<d:propfind xmlns:d="DAV:" xmlns:cs="https://calendarserver.org/ns/"><d:prop><d:resourcetype /><d:displayname /></d:prop></d:propfind>' $DAVSERVER/principals/user/$USER/
-# # | xmllint -format -
-
-# # curl --user "$CREDENTIALS" -sD /dev/stderr -H "Content-Type: application/xml" -X PROPFIND -H "Depth: infinity" --data '<d:propfind xmlns:d="DAV:" xmlns:cs="https://calendarserver.org/ns/"><d:prop><d:resourcetype /><d:displayname /></d:prop></d:propfind>' $DAVSERVER/calendars/user/$USER/ | xmllint -format -
-
+#TODO as  a user, all of those
+# /home/mollekopf/bin/scripts/imapcli.rb delete --host $HOST --port 993 --username "$USERNAME" --password "$PASSWORD" --ssl --debug $FOLDER || true
+# /home/mollekopf/bin/scripts/imapcli.rb create --host $HOST --port 993 --username "$USERNAME" --password "$PASSWORD" --ssl --debug $FOLDER
+# /home/mollekopf/bin/scripts/imapcli.rb subscribe --host $HOST --port 993 --username "$USERNAME" --password "$PASSWORD" --ssl --debug $FOLDER
+# /home/mollekopf/bin/scripts/imapcli.rb setmetadata --host $HOST --port 993 --username "$USERNAME" --password "$PASSWORD" --ssl --debug $FOLDER "/shared/vendor/kolab/folder-type" event
+# /home/mollekopf/bin/scripts/imapcli.rb getmetadata --host $HOST --port 993 --username "$USERNAME" --password "$PASSWORD" --ssl --debug $FOLDER "/shared/*"
+# /home/mollekopf/bin/scripts/imapcli.rb getmetadata --host $HOST --port 993 --username "$USERNAME" --password "$PASSWORD" --ssl --debug $FOLDER "/shared/vendor/kolab/folder-type"
+# /home/mollekopf/bin/scripts/imapcli.rb lsub --host $HOST --port 993 --username "$USERNAME" --password "$PASSWORD" --ssl --debug $FOLDER
+# /home/mollekopf/bin/scripts/imapcli.rb list --host $HOST --port 993 --username "$USERNAME" --password "$PASSWORD" --ssl --debug $FOLDER
+# /home/mollekopf/bin/scripts/imapcli.rb delete --host $HOST --port 993 --username "$USERNAME" --password "$PASSWORD" --ssl --debug $FOLDER
