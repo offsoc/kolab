837a8b86a6d4e8ce1135f7a3d52a4e1cf6310570
Christian Mollekopf <mollekopf@apheleia-it.ch>
2023-11-15 13:26:26 +0100

    Configure the allowed hosts for collabora

--- docker/collabora/init.sh
+++ docker/collabora/init.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 
 
+GROUPSSTRING=""
+for HOST in $ALLOWED_HOSTS; do
+    GROUPSSTRING="$GROUPSSTRING<group><host desc=\"hostname to allow or deny.\" allow=\"true\">$HOST</host></group>\n"
+done
+
+sed -i -e "s|ALLOWED_HOSTS_GROUPS|$GROUPSSTRING|" /etc/coolwsd/coolwsd.xml
+
 mkdir -p /tmp/ssl/
 pushd /tmp/ssl/
 mkdir -p certs/ca
 openssl rand -writerand /opt/cool/.rnd
 openssl genrsa -out certs/ca/root.key.pem 2048
 openssl req -x509 -new -nodes -key certs/ca/root.key.pem -days 9131 -out certs/ca/root.crt.pem -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=Dummy Authority"
-mkdir -p certs/servers
 mkdir -p certs/tmp
 mkdir -p certs/servers/localhost
 openssl genrsa -out certs/servers/localhost/privkey.pem 2048
 if test "${cert_domain-set}" = set; then
-openssl req -key certs/servers/localhost/privkey.pem -new -sha256 -out certs/tmp/localhost.csr.pem -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=localhost"
+    openssl req -key certs/servers/localhost/privkey.pem -new -sha256 -out certs/tmp/localhost.csr.pem -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=localhost"
 else
-openssl req -key certs/servers/localhost/privkey.pem -new -sha256 -out certs/tmp/localhost.csr.pem -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=${cert_domain}"
+    openssl req -key certs/servers/localhost/privkey.pem -new -sha256 -out certs/tmp/localhost.csr.pem -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=${cert_domain}"
 fi
 openssl x509 -req -in certs/tmp/localhost.csr.pem -CA certs/ca/root.crt.pem -CAkey certs/ca/root.key.pem -CAcreateserial -out certs/servers/localhost/cert.pem -days 9131
 mv -f certs/servers/localhost/privkey.pem /etc/coolwsd/key.pem
