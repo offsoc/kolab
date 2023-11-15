e854e39bb9f12850dbcff51a6084767dc45edd3d
Christian Mollekopf <mollekopf@apheleia-it.ch>
2023-11-15 14:58:42 +0100

    A kolabctl script as entrypoint for end users

--- bin/selfcheck.sh
+++ bin/selfcheck.sh
@@ -12,3 +12,5 @@ docker compose exec imap testsaslauthd -u "admin@$APP_DOMAIN" -p "$ADMIN_PASSWOR
 utils/mailtransporttest.py --sender-username "admin@$APP_DOMAIN" --sender-password "$ADMIN_PASSWORD" --sender-host "$APP_DOMAIN" --recipient-username "admin@$APP_DOMAIN" --recipient-password "$ADMIN_PASSWORD" --recipient-host "$APP_DOMAIN"
 
 utils/kolabendpointtester.py --verbose --host "$APP_DOMAIN" --dav "https://$APP_DOMAIN/dav/" --imap "$APP_DOMAIN" --activesync "$APP_DOMAIN"  --user "admin@$APP_DOMAIN" --password "$ADMIN_PASSWORD"
+
+echo "All tests have passed!"
