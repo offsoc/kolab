0f792adeecec0acaa6cebe26bb60d31826e4daf9
Christian Mollekopf <mollekopf@apheleia-it.ch>
2023-11-22 15:59:10 +0100

    We shouldn't be setting these
    And if we did, that's not how we can reference a dav collection.

--- docker/roundcube/rootfs/opt/app-root/src/roundcubemail-config-templates/config.inc.php
+++ docker/roundcube/rootfs/opt/app-root/src/roundcubemail-config-templates/config.inc.php
@@ -181,8 +181,6 @@
 
     // Disable the default addressbook and use the dav addressbook by default
     $config['address_book_type'] = '';
-    $config['default_addressbook'] = 'Addressbook';
-    $config['autocomplete_addressbooks'] = array('Addressbook');
 
     $config['autocomplete_single'] = true;
 
