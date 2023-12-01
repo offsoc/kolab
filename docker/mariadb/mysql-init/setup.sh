b36e525e33fdeef549dd7981e9285275872f33bb
Christian Mollekopf <mollekopf@apheleia-it.ch>
2023-12-01 21:16:57 +0100

    Preseed the roundcube database
    Create roundcube tables for backend so we don't have a circular
    dependency
    
    Webapp depends on roundcube for db seed, and roundcube depends on webapp
    for authentication.

--- docker/mariadb/mysql-init/setup.sh
+++ docker/mariadb/mysql-init/setup.sh
@@ -15,6 +15,66 @@ GRANT ALL PRIVILEGES ON ${DB_KOLAB_DATABASE}.* TO ${DB_KOLAB_USERNAME}@'%' IDENT
 FLUSH PRIVILEGES;
 EOF
 
+MYSQL_PWD=$MARIADB_ROOT_PASSWORD mysql --protocol=socket -uroot -hlocalhost --socket="/run/mysqld/mysqld.sock" << EOF
+CREATE DATABASE IF NOT EXISTS $DB_RC_DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER IF NOT EXISTS $DB_RC_USERNAME@'%' IDENTIFIED BY '$DB_RC_PASSWORD';
+ALTER USER $DB_RC_USERNAME@'%' IDENTIFIED BY '$DB_RC_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_RC_DATABASE.* TO $DB_RC_USERNAME@'%';
+FLUSH PRIVILEGES;
+EOF
+
+MYSQL_PWD=$DB_RC_PASSWORD mysql --protocol=socket -uroot -hlocalhost --socket="/run/mysqld/mysqld.sock" ${DB_RC_USERNAME}<< EOF
+CREATE TABLE users (
+ user_id int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+ username varchar(128) BINARY NOT NULL,
+ mail_host varchar(128) NOT NULL,
+ created datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+ last_login datetime DEFAULT NULL,
+ failed_login datetime DEFAULT NULL,
+ failed_login_counter int(10) UNSIGNED DEFAULT NULL,
+ language varchar(16),
+ preferences longtext,
+ PRIMARY KEY(user_id),
+ UNIQUE username (username, mail_host)
+) ROW_FORMAT=DYNAMIC ENGINE=INNODB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+CREATE TABLE identities (
+ identity_id int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+ user_id int(10) UNSIGNED NOT NULL,
+ changed datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+ del tinyint(1) NOT NULL DEFAULT '0',
+ standard tinyint(1) NOT NULL DEFAULT '0',
+ name varchar(128) NOT NULL,
+ organization varchar(128) NOT NULL DEFAULT '',
+ email varchar(128) NOT NULL,
+ reply-to varchar(128) NOT NULL DEFAULT '',
+ bcc varchar(128) NOT NULL DEFAULT '',
+ signature longtext,
+ html_signature tinyint(1) NOT NULL DEFAULT '0',
+ PRIMARY KEY(identity_id),
+ CONSTRAINT user_id_fk_identities FOREIGN KEY (user_id)
+   REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+ INDEX user_identities_index (user_id, del),
+ INDEX email_identities_index (email, del)
+) ROW_FORMAT=DYNAMIC ENGINE=INNODB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+CREATE TABLE filestore (
+ file_id int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+ user_id int(10) UNSIGNED NOT NULL,
+ context varchar(32) NOT NULL,
+ filename varchar(128) NOT NULL,
+ mtime int(10) NOT NULL,
+ data longtext NOT NULL,
+ PRIMARY KEY (file_id),
+ CONSTRAINT user_id_fk_filestore FOREIGN KEY (user_id)
+   REFERENCES users (user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+ UNIQUE uniqueness (user_id, context, filename)
+) ROW_FORMAT=DYNAMIC ENGINE=INNODB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+EOF
+
 # Powerdns setup according to https://github.com/PowerDNS/pdns/blob/master/modules/gmysqlbackend/schema.mysql.sql
 # Required for the first boot, afterwards the laravel migration will take over.
 # This is only required so pdns can start cleanly, indexes etc are handled by the laravel migration.
