5a7f1220972e4478021e784a778b014d1cf79a62
Christian Mollekopf <mollekopf@apheleia-it.ch>
2024-01-25 23:00:09 +0100

    almalinux base images
    By using base images we:
    * make sure we only have to transfer the base layers once
    * reduce the complexity of the individual dockerfiles
    * include common troubleshooting tools in all containers
    
    It does make the build complex slightly more complex, but that's
    managable.

--- bin/build.sh
+++ bin/build.sh
@@ -2,6 +2,6 @@
 
 # Workaround because docker-compose doesn't know build dependencies, so we build the dependencies first
 # (It does respect depends_on, but we don't actually want the dependencies started, so....)
-docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.build.yml build swoole
-docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.build.yml build webapp
-docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.build.yml build
+docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.build.yml build $@ swoole almalinux8 almalinux9
+docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.build.yml build $@ webapp
+docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.build.yml build $@
--- bin/update.sh
+++ bin/update.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 docker compose down --remove-orphans
-docker compose pull --ignore-buildable
+
+docker pull quay.io/sclorg/mariadb-105-c9s
+docker pull minio/minio:latest
+docker pull almalinux:8
+docker pull almalinux:9
+docker pull fedora:35
+docker pull fedora:37
+
 bin/reconfigure.sh
 docker compose build
 bin/regen-certs
