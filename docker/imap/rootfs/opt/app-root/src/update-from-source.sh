8519b8637923bea9d57ec0e4274630423b9df3eb
Christian Mollekopf <mollekopf@apheleia-it.ch>
2024-03-05 15:31:25 +0100

    Disable ssl on imap and make cyrus-imap buildable from injected source
    Primarily so we can easily experiment with patches, doesn't really
    change what we run in production.

---
+++ docker/imap/rootfs/opt/app-root/src/update-from-source.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Update from source (rather than via composer which updates to the latest commit)
+
+rsync -av \
+    --no-links \
+    --exclude=.git \
+    --exclude='*.o' \
+    --exclude='*.Plo' \
+    --exclude='*.lo' \
+    /src.orig/cyrus-imapd/ /opt/app-root/src/cyrus-imapd
+
+pushd /opt/app-root/src/cyrus-imapd
+autoreconf -i
+./configure CFLAGS="-W -Wno-unused-parameter -g -O0 -Wall -Wextra -Werror -fPIC" --enable-murder --enable-http --enable-calalarmd --enable-autocreate --enable-idled --with-openssl=yes --enable-replication --prefix=/usr
+make -j6
+make install
+popd
+
+./reload.sh
