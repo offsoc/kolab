3c0416ffd5ce301c322bccc78c1ad45d34f26b1e
Christian Mollekopf <mollekopf@apheleia-it.ch>
2024-04-02 15:01:33 +0200

    Update the imap container to almalinux9

--- docker/imap/build.sh
+++ docker/imap/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 function checkout() {
     if [ ! -d "$1" ]; then
         git clone "$2" "$1"
@@ -12,7 +14,13 @@ function checkout() {
 checkout cyrus-imapd $GIT_REMOTE $GIT_REF
 pushd cyrus-imapd
 autoreconf -i
-./configure CFLAGS="-W -Wno-unused-parameter -g -O0 -Wall -Wextra -Werror -fPIC" --enable-murder --enable-http --enable-calalarmd --enable-autocreate --enable-idled --with-openssl=yes --enable-replication --prefix=/usr
+
+#-Wno-deprecated to work around the following:
+#imap/http_h2.c:767:13: error: ‘MD5_Final’ is deprecated: Since OpenSSL 3.0 [-Werror=deprecated-declarations]
+# imap/ctl_mboxlist.c:997:21: error: ‘free’ called on pointer ‘entry’ with nonzero offset 2052 [-Werror=free-nonheap-object]
+./configure CFLAGS="-W -Wno-unused-parameter -g -O0 -Wall -Wextra -Werror -Wno-error=deprecated-declarations  -Wno-error=free-nonheap-object -fPIC" --enable-murder --enable-http --enable-calalarmd --enable-autocreate --enable-idled --with-openssl=yes --enable-replication --prefix=/usr
+
+
 make -j6
 make install
 popd
