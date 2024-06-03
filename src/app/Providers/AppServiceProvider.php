1e7d7cf82a5b0586d31c77965ecbf4b119b0a06a
Christian Mollekopf <mollekopf@apheleia-it.ch>
2024-06-03 15:47:12 +0200

    Revert "Laravel Passport v12"
    This reverts commit 7ea42bc4f65a4a478d969c6124e04edbccbbc081.
    
    We need to sort out the libsodium issues first

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        Passport::enablePasswordGrant();
+        Passport::ignoreMigrations();
         Passport::ignoreRoutes();
     }
 
