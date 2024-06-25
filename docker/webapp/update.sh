68aecc793185b267adb60403f43aaaa936da9861
Christian Mollekopf <mollekopf@apheleia-it.ch>
2024-06-25 15:29:00 +0200

    update-source.sh

--- docker/webapp/update.sh
+++ docker/webapp/update.sh
@@ -2,17 +2,7 @@
 set -e
 set -x
 
-rsync -av \
-    --exclude=vendor \
-    --exclude=composer.lock \
-    --exclude=node_modules \
-    --exclude=package-lock.json \
-    --exclude=public \
-    --exclude=storage \
-    --exclude=resources/build \
-    --exclude=bootstrap \
-    --exclude=.gitignore \
-    /src/kolabsrc.orig/ /opt/app-root/src/ | tee /tmp/rsync.output
+/update-source.sh
 
 cd /opt/app-root/src/
 
