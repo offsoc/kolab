77f9db11fec667d1a6eab76c7d2ee663f931973e
Christian Mollekopf <mollekopf@apheleia-it.ch>
2024-07-03 11:26:38 +0200

    Don't run bin/build.sh when it is just being sourced
    and source the correct location

--- bin/build.sh
+++ bin/build.sh
@@ -48,32 +48,35 @@ podman__build_roundcube() {
         ${GIT_REF_FREEBUSY:+"--build-arg=GIT_REF_FREEBUSY=$GIT_REF_FREEBUSY"}
 }
 
-if [[ $1 == "--podman" ]]; then
-    echo "Building with podman"
-    shift
-    podman__build_base
-    podman__build_webapp
-    podman__build_meet
-    podman build docker/postfix -t kolab-postfix
-    podman build docker/imap -t kolab-imap
-        ${IMAP_GIT_REMOTE:+"--build-arg=GIT_REMOTE=$IMAP_GIT_REMOTE"} \
-        ${IMAP_GIT_REF:+"--build-arg=GIT_REF=$IMAP_GIT_REF"}
-    podman build docker/amavis -t kolab-amavis
-    podman build docker/collabora -t kolab-collabora --build-arg=REPOSITORY="https://www.collaboraoffice.com/repos/CollaboraOnline/23.05-CODE/CODE-rpm/"
-    podman build docker/mariadb -t mariadb
-    podman build docker/redis -t redis
-    podman build docker/proxy -t kolab-proxy
-    podman build docker/coturn -t kolab-coturn
-    podman build docker/utils -t kolab-utils
-    podman build docker/fluentbit -t fluentbit
-    podman__build_roundcube
-else
-    echo "Building with docker compose"
-    # Workaround because docker-compose doesn't know build dependencies, so we build the dependencies first
-    # (It does respect depends_on, but we don't actually want the dependencies started, so....)
-    docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.build.yml build $@ almalinux9
-    docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.build.yml build $@ swoole
-    docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.build.yml build $@ webapp
-    docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.build.yml build $@
+# Avoid executing when sourcing the script
+if [[ "${#BASH_SOURCE[@]}" -eq 1 ]]; then
+    if [[ $1 == "--podman" ]]; then
+        echo "Building with podman"
+        shift
+        podman__build_base
+        podman__build_webapp
+        podman__build_meet
+        podman build docker/postfix -t kolab-postfix
+        podman build docker/imap -t kolab-imap
+            ${IMAP_GIT_REMOTE:+"--build-arg=GIT_REMOTE=$IMAP_GIT_REMOTE"} \
+            ${IMAP_GIT_REF:+"--build-arg=GIT_REF=$IMAP_GIT_REF"}
+        podman build docker/amavis -t kolab-amavis
+        podman build docker/collabora -t kolab-collabora --build-arg=REPOSITORY="https://www.collaboraoffice.com/repos/CollaboraOnline/23.05-CODE/CODE-rpm/"
+        podman build docker/mariadb -t mariadb
+        podman build docker/redis -t redis
+        podman build docker/proxy -t kolab-proxy
+        podman build docker/coturn -t kolab-coturn
+        podman build docker/utils -t kolab-utils
+        podman build docker/fluentbit -t fluentbit
+        podman__build_roundcube
+    else
+        echo "Building with docker compose"
+        # Workaround because docker-compose doesn't know build dependencies, so we build the dependencies first
+        # (It does respect depends_on, but we don't actually want the dependencies started, so....)
+        docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.build.yml build $@ almalinux9
+        docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.build.yml build $@ swoole
+        docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.build.yml build $@ webapp
+        docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.build.yml build $@
+    fi
 fi
 
