a1376331c72067373465dc05d800e4765949ccee
Jeroen van Meeuwen <vanmeeuwen@apheleia-it.ch>
2024-10-02 12:56:57 +0200

    Correct use statements, attempt 2

--- src/app/Http/Controllers/API/V4/LicenseController.php
+++ src/app/Http/Controllers/API/V4/LicenseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\V4;
 
 use App\Http\Controllers\Controller;
+use App\License;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
