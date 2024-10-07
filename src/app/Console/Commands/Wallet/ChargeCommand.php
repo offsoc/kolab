34e0a99f37a534e71bddd4e6345e9b09b05d0a99
Christian Mollekopf <mollekopf@apheleia-it.ch>
2024-10-07 09:14:25 +0200

    --topup mode for wallet:charge
    Summary:
    This mode just finds wallets that need a topup, and triggers a
    WalletCharge job for them.
    
    Reviewers: #hkccp_developers, machniak
    
    Reviewed By: #hkccp_developers, machniak
    
    Subscribers: machniak
    
    Differential Revision: https://git.kolab.org/D4971

--- src/app/Console/Commands/Wallet/ChargeCommand.php
+++ src/app/Console/Commands/Wallet/ChargeCommand.php
@@ -11,14 +11,14 @@ class ChargeCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'wallet:charge {wallet?}';
+    protected $signature = 'wallet:charge {--topup : Only top-up wallets} {--dry-run} {wallet?}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Charge wallets';
+    protected $description = 'Charge wallets, and trigger a topup on charged wallets.';
 
     /**
      * Execute the console command.
@@ -42,6 +42,22 @@ class ChargeCommand extends Command
             }
 
             $wallets = [$wallet];
+        } elseif ($this->option('topup')) {
+            // Find wallets that need to be topped up
+            $wallets = \App\Wallet::select('wallets.id')
+                ->join('users', 'users.id', '=', 'wallets.user_id')
+                ->join('wallet_settings', function (\Illuminate\Database\Query\JoinClause $join) {
+                    $join->on('wallet_settings.wallet_id', '=', 'wallets.id')
+                        ->where('wallet_settings.key', '=', 'mandate_balance');
+                })
+                ->whereNull('users.deleted_at')
+                ->whereRaw('wallets.balance < (wallet_settings.value * 100)')
+                ->whereNot(
+                    'users.status',
+                    '&',
+                    \App\User::STATUS_DEGRADED | \App\User::STATUS_SUSPENDED | \App\User::STATUS_DELETED
+                )
+                ->cursor();
         } else {
             // Get all wallets, excluding deleted accounts
             $wallets = \App\Wallet::select('wallets.id')
@@ -51,7 +67,15 @@ class ChargeCommand extends Command
         }
 
         foreach ($wallets as $wallet) {
-            \App\Jobs\WalletCheck::dispatch($wallet->id);
+            if ($this->option('dry-run')) {
+                $this->info($wallet->id);
+            } else {
+                if ($this->option('topup')) {
+                    \App\Jobs\WalletCharge::dispatch($wallet->id);
+                } else {
+                    \App\Jobs\WalletCheck::dispatch($wallet->id);
+                }
+            }
         }
     }
 }
