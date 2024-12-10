7cdc7e18a9bf8726fcca6f9a9c603e8ff5a210de
Christian Mollekopf <mollekopf@apheleia-it.ch>
2024-12-10 09:04:00 +0100

    We deploy from master, so list changes from master

--- bin/filterchangelog.sh
+++ bin/filterchangelog.sh
@@ -3,10 +3,10 @@ echo "Listing changes since commit $1"
 
 echo
 echo "Kolab 4 changes: "
-git log --oneline $1..HEAD -- src/
+git log --oneline $1..master -- src/
 echo
 echo "Container changes: "
-git log --oneline $1..HEAD -- docker
+git log --oneline $1..master -- docker
 echo
 echo "Migrations: "
-git log --oneline $1..HEAD -- src/database/migrations/
+git log --oneline $1..master -- src/database/migrations/
