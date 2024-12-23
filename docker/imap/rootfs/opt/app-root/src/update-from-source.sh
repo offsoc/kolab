e043f49f964253f61dc27dad9ca66b01eaffb9b5
Christian Mollekopf <mollekopf@apheleia-it.ch>
2024-12-23 09:43:12 +0100

    Make it possible to rebuild cyrus again
    We should have a better approach as this creates an expensive layer
    copying all of /usr

--- docker/imap/rootfs/opt/app-root/src/update-from-source.sh
+++ docker/imap/rootfs/opt/app-root/src/update-from-source.sh
@@ -11,7 +11,7 @@ rsync -av \
 
 pushd /opt/app-root/src/cyrus-imapd
 autoreconf -i
-./configure CFLAGS="-W -Wno-unused-parameter -g -O0 -Wall -Wextra -Werror -fPIC" --enable-murder --enable-http --enable-calalarmd --enable-autocreate --enable-idled --with-openssl=yes --enable-replication --prefix=/usr
+./configure CFLAGS="-W -Wno-unused-parameter -Wno-error=deprecated-declarations -g -O0 -Wall -Wextra -Werror -fPIC" --enable-murder --enable-http --enable-calalarmd --enable-autocreate --enable-idled --with-openssl=yes --enable-replication --prefix=/usr
 make -j6
 make install
 popd
