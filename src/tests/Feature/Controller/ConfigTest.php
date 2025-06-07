d90cc02b11c37ecba73ce363bcbc05bdb160ef0c
Aleksander Machniak <machniak@kolabsys.com>
2025-06-07 09:26:32 +0200

    Fix test regression

--- src/tests/Feature/Controller/ConfigTest.php
+++ src/tests/Feature/Controller/ConfigTest.php
@@ -23,32 +23,22 @@ class ConfigTest extends TestCase
 
         $json = $response->json();
 
-        $this->assertContains('kolab_files', $json['plugins']);
-        $this->assertContains('kolab_tags', $json['plugins']);
-        $this->assertContains('calendar', $json['plugins']);
-        $this->assertContains('tasklist', $json['plugins']);
+        $this->assertSame(['kolab4', 'groupware'], $json['kolab-configuration-overlays']);
 
+        // Ned has groupware, activesync and 2FA
         $response = $this->actingAs($ned)->get('api/v4/config/webmail');
         $response->assertStatus(200);
 
         $json = $response->json();
 
-        $this->assertContains('kolab_2fa', $json['plugins']);
-        $this->assertContains('kolab_activesync', $json['plugins']);
-        $this->assertContains('kolab_files', $json['plugins']);
-        $this->assertContains('kolab_tags', $json['plugins']);
-        $this->assertContains('calendar', $json['plugins']);
-        $this->assertContains('tasklist', $json['plugins']);
+        $this->assertSame(['kolab4', 'activesync', '2fa', 'groupware'], $json['kolab-configuration-overlays']);
 
+        // Joe has no groupware subscription
         $response = $this->actingAs($joe)->get('api/v4/config/webmail');
         $response->assertStatus(200);
 
         $json = $response->json();
 
-        $this->assertSame([], $json['plugins']);
-        $this->assertTrue($json['calendar_disabled']);
-        $this->assertTrue($json['kolab_files_disabled']);
-        $this->assertTrue($json['kolab_tags_disabled']);
-        $this->assertTrue($json['tasklist_disabled']);
+        $this->assertSame(['kolab4'], $json['kolab-configuration-overlays']);
     }
 }
